b2e66f4eb0047535399a95fcd9c3c3fa8c8a7b0b
falko <falko@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2006-01-02 12:59:04 +0000

    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@60 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/dns/rr_edit.php
+++ interface/web/dns/rr_edit.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Copyright (c) 2005, Till Brehm, projektfarm Gmbh
+Copyright (c) 2005, Till Brehm, Falko Timme, projektfarm Gmbh
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
