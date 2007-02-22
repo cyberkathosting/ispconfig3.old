7106ae4850369d7af1782d1bdc556fde7ae998e5
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2007-02-22 16:22:09 +0000

    cleanup
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/branches/MyDNSConfig@78 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- trunk/interface/sql/auth_sql_snippet.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `mail_domain` ADD `sys_userid` INT NOT NULL AFTER `domain_id` ,
-ADD `sys_groupid` INT NOT NULL AFTER `sys_userid` ,
-ADD `sys_perm_user` VARCHAR( 5 ) NOT NULL AFTER `sys_groupid` ,
-ADD `sys_perm_group` VARCHAR( 5 ) NOT NULL AFTER `sys_perm_user` ,
-ADD `sys_perm_other` VARCHAR( 5 ) NOT NULL AFTER `sys_perm_group` ;
