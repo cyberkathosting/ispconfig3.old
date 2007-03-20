f7365e4a3fcb3d939953fe82afba7d4c62fde41c
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2007-03-20 21:47:04 +0000

    cleanup in config file. The path is now detected automatically.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@90 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/lib/config.inc.php
+++ interface/lib/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Copyright (c) 2005, Till Brehm, Falko Timme, projektfarm Gmbh
+Copyright (c) 2007, Till Brehm, Falko Timme, projektfarm Gmbh
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
@@ -37,38 +37,37 @@ $conf["app_title"] = "ISPConfig";
 $conf["app_version"] = "3.0.0";
 
 
-$conf["rootpath"]			= "D:\\server\\www\\ispconfig3_ajax\\interface";
-//$conf["rootpath"]			= "D:\\www\\ispconfig3\\interface";
-//$conf["rootpath"]			= "/home/www/ispconfig3/web/cms";
-
-$conf["fs_div"]				= "\\"; // File system divider, \\ on windows and / on linux and unix
-$conf["classpath"]			= $conf["rootpath"].$conf["fs_div"]."lib".$conf["fs_div"]."classes";
-$conf["temppath"]			= $conf["rootpath"].$conf["fs_div"]."temp";
-
-
-define("DIR_TRENNER","\\");
-define("SERVER_ROOT","D:\\server\\www\\ispconfig3_ajax\\interface");
-define("INCLUDE_ROOT",SERVER_ROOT.DIR_TRENNER."lib");
-define("CLASSES_ROOT",INCLUDE_ROOT.DIR_TRENNER."classes");
-
 /*
         Database Settings
 */
-
-define("DB_TYPE","mysql");
-define("DB_HOST","localhost");
-define("DB_DATABASE","ispconfig3");
-define("DB_USER","root");
-define("DB_PASSWORD","");
-
-
-
 
 $conf["db_type"]			= 'mysql';
 $conf["db_host"]			= 'localhost';
 $conf["db_database"]		= 'ispconfig3';
 $conf["db_user"]			= 'root';
 $conf["db_password"]		= '';
+
+
+/*
+   Path Settings (Do not change!)
+*/
+
+$conf["rootpath"]			= substr(dirname(__FILE__),0,-4);
+$conf["fs_div"]				= "/"; // File system divider, \\ on windows and / on linux and unix
+$conf["classpath"]			= $conf["rootpath"].$conf["fs_div"]."lib".$conf["fs_div"]."classes";
+$conf["temppath"]			= $conf["rootpath"].$conf["fs_div"]."temp";
+
+
+define("DIR_TRENNER",$conf["fs_div"]);
+define("SERVER_ROOT",$conf["rootpath"]);
+define("INCLUDE_ROOT",SERVER_ROOT.DIR_TRENNER."lib");
+define("CLASSES_ROOT",INCLUDE_ROOT.DIR_TRENNER."classes");
+
+define("DB_TYPE",$conf["db_type"]);
+define("DB_HOST",$conf["db_host"]);
+define("DB_DATABASE",$conf["db_database"]);
+define("DB_USER",$conf["db_user"]);
+define("DB_PASSWORD",$conf["db_password"]);
 
 
 /*
@@ -83,9 +82,9 @@ $conf["programs"]["wput"]		= $conf["rootpath"]."\\tools\\wput\\wput.exe";
         Themes
 */
 
-$conf["theme"]									= 'default';
+$conf["theme"]					= 'default';
 $conf["html_content_encoding"]	= 'text/html; charset=iso-8859-1';
-$conf["logo"] 									= 'themes/default/images/mydnsconfig_logo.gif';
+$conf["logo"] 					= 'themes/default/images/mydnsconfig_logo.gif';
 
 /*
         Default Language
@@ -98,20 +97,20 @@ $conf["language"]                = 'en';
         Auto Load Modules
 */
 
-$conf["start_db"]				= true;
+$conf["start_db"]		= true;
 $conf["start_session"]	= true;
 
 /*
         DNS Settings
 */
 
-$conf["auto_create_ptr"] = 1; // Automatically create PTR records?
-$conf["default_ns"] = 'ns1.example.com.'; // must be set if $conf['auto_create_ptr'] is 1. Don't forget the trailing dot!
-$conf["default_mbox"] = 'admin.example.com.'; // Admin email address. Must be set if $conf['auto_create_ptr'] is 1. Replace "@" with ".". Don't forget the trailing dot!
-$conf["default_ttl"] = 86400;
-$conf["default_refresh"] = 28800;
-$conf["default_retry"] = 7200;
-$conf["default_expire"] = 604800;
+$conf["auto_create_ptr"] 	 = 1; // Automatically create PTR records?
+$conf["default_ns"] 		 = 'ns1.example.com.'; // must be set if $conf['auto_create_ptr'] is 1. Don't forget the trailing dot!
+$conf["default_mbox"] 		 = 'admin.example.com.'; // Admin email address. Must be set if $conf['auto_create_ptr'] is 1. Replace "@" with ".". Don't forget the trailing dot!
+$conf["default_ttl"] 		 = 86400;
+$conf["default_refresh"] 	 = 28800;
+$conf["default_retry"] 		 = 7200;
+$conf["default_expire"] 	 = 604800;
 $conf["default_minimum_ttl"] = 86400;
 
 ?>
