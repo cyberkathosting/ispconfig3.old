730354bb65f620918caaf21a57572c376f2bd0c0
pedro_morgan <pedro_morgan@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2007-08-18 01:17:37 +0000

    Code tidy and janitor
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@157 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/lib/config.inc.php
+++ interface/lib/config.inc.php
@@ -27,10 +27,10 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-Header('Pragma: no-cache');
-Header('Cache-Control: no-store, no-cache, max-age=0, must-revalidate');
-Header('Content-Type: text/html');
-
+header('Pragma: no-cache');
+header('Cache-Control: no-store, no-cache, max-age=0, must-revalidate');
+header('Content-Type: text/html');
+//* TODO: Js caching - pedro
 
 //** Key paramaters
 $conf['app_title'] = 'ISPConfig';
@@ -38,32 +38,75 @@ $conf['app_version'] = '3.0.0';
 $conf['modules_available'] 	= 'admin,mail,sites,monitor,client,dns';
 
 
-//** Database Settings
-$conf['db_type']			= 'mysql';
-$conf['db_host']			= 'localhost';
-$conf['db_database']		= 'ispconfig3';
-$conf['db_user']			= 'root';
-$conf['db_password']		= '';
+//** Future Code  - pedro comments
+/* Database connection
+	The only time its needed is to connectm otherwise it not needed as 
+	variables aronf the application. Connected and done.
+	Prefered is an array as $DB in capitals, ie
+	$DB['host'] = 'type'; 
+	$DB['host'] = 'ip';
+	$DB['user'] = 'me';
+	$DB['password'] = 'secret';
+	$DB['database'] = 'persistent_data_stash';
+	
+	The connection paramaters are all contained within one array structure
+	With this array structure the connection can be passed around, to functions
+	However it can also leak. and be destroyed eg
+	$db->connect($DB);
+	unset($DB); // only the paranoid survive
+		
+	Also there is a scenario where we are devloping and using this file
+	and the database paramaters might leak.
+	To resolve this there is a file called db_local.php.skel which is not detected
+	rename this to db_local.php and edit the paramaters
+	
+	$DB['type']			= 'mysql';
+	$DB['host']			= 'localhost';
+	$DB['database']		= 'ispconfig3';
+	$DB['user']			= 'root';
+	$DB['password']		= '';
+
+	
+*/
+
+//** Detect for local database setting or set and load default params
+if( file_exists('db_local.php') ){
+	require_once('db_local.php');
+	$conf['db_type']			= $DB['type'];
+	$conf['db_host']			= $DB['host'];
+	$conf['db_database']		= $DB['database'];
+	$conf['db_user']			= $DB['user'];
+	$conf['db_password']		= $DB['password'];	
+}else{
+	//** Database Settings
+	$conf['db_type']			= 'mysql';
+	$conf['db_host']			= 'localhost';
+	$conf['db_database']		= 'ispconfig3';
+	$conf['db_user']			= 'root';
+	$conf['db_password']		= '';
+}
 
 
 //** Path Settings (Do not change!)
-$conf["rootpath"]			= substr(dirname(__FILE__),0,-4);
-$conf["fs_div"]				= '/'; // File system divider, \\ on windows and / on linux and unix
-$conf["classpath"]			= $conf['rootpath'].$conf['fs_div'].'lib'.$conf['fs_div'].'classes';
-$conf["temppath"]			= $conf['rootpath'].$conf['fs_div'].'temp';
+$conf['rootpath']			= substr(dirname(__FILE__),0,-4);
+$conf['fs_div']				= '/'; // File system divider, \\ on windows and / on linux and unix
+$conf['classpath']			= $conf['rootpath'].$conf['fs_div'].'lib'.$conf['fs_div'].'classes';
+$conf['temppath']			= $conf['rootpath'].$conf['fs_div'].'temp';
 
 
-define('DIR_TRENNER',$conf['fs_div']);
-define('SERVER_ROOT',$conf['rootpath']);
-define('INCLUDE_ROOT',SERVER_ROOT.DIR_TRENNER.'lib');
-define('CLASSES_ROOT',INCLUDE_ROOT.DIR_TRENNER.'classes');
+define('DIR_TRENNER', $conf['fs_div']);
+define('SERVER_ROOT', $conf['rootpath']);
+define('INCLUDE_ROOT', SERVER_ROOT.DIR_TRENNER.'lib');
+define('CLASSES_ROOT', INCLUDE_ROOT.DIR_TRENNER.'classes');
 
+/* pedro notes ? this stuff is REALLY not necessay */
+/*
 define('DB_TYPE', $conf['db_type']);
 define('DB_HOST', $conf['db_host']);
 define('DB_DATABASE',$conf['db_database']);
 define('DB_USER', $conf['db_user']);
 define('DB_PASSWORD', $conf['db_password']);
-
+*/
 
 //**  External programs
 //$conf["programs"]["convert"]	= "/usr/bin/convert";
