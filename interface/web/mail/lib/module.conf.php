87eb7cae1e264be6458b2b4a285635c83c2f5858
pedro_morgan <pedro_morgan@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2007-08-19 20:11:45 +0000

    Tidy up of mail/module.conf. replaced unset() as its faster, =array() muted E_NOTICE
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@189 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/mail/lib/module.conf.php
+++ interface/web/mail/lib/module.conf.php
@@ -1,118 +1,101 @@
 <?php
 
-$module["name"] 		= "mail";
-$module["title"] 		= "Email";
-$module["template"] 	= "module.tpl.htm";
-$module["startpage"] 	= "mail/mail_domain_list.php";
-$module["tab_width"]    = '';
+$module['name'] 		= 'mail';
+$module['title'] 		= 'Email';
+$module['template'] 	= 'module.tpl.htm';
+$module['startpage'] 	= 'mail/mail_domain_list.php';
+$module['tab_width']    = '';
 
-/*
-	Email accounts menu
-*/
 
-$items[] = array( 'title' 	=> "Domain",
+//**** Email accounts menu
+$items = array();
+
+$items[] = array( 'title' 	=> 'Domain',
 				  'target' 	=> 'content',
 				  'link'	=> 'mail/mail_domain_list.php');
 
-$items[] = array( 'title' 	=> "Email Mailbox",
+$items[] = array( 'title' 	=> 'Email Mailbox',
 				  'target' 	=> 'content',
 				  'link'	=> 'mail/mail_user_list.php');
 
-$items[] = array( 'title' 	=> "Email Alias",
+$items[] = array( 'title' 	=> 'Email Alias',
 				  'target' 	=> 'content',
 				  'link'	=> 'mail/mail_alias_list.php');			  
 
-$items[] = array( 'title' 	=> "Email Forward",
+$items[] = array( 'title' 	=> 'Email Forward',
 				  'target' 	=> 'content',
 				  'link'	=> 'mail/mail_forward_list.php');
 
-$items[] = array( 'title' 	=> "Email Catchall",
+$items[] = array( 'title' 	=> 'Email Catchall',
 				  'target' 	=> 'content',
 				  'link'	=> 'mail/mail_domain_catchall_list.php');
 
-$items[] = array( 'title' 	=> "Email Routing",
+$items[] = array( 'title' 	=> 'Email Routing',
 				  'target' 	=> 'content',
 				  'link'	=> 'mail/mail_transport_list.php');
 
-$module["nav"][] = array(	'title'	=> 'Email Accounts',
+$module['nav'][] = array(	'title'	=> 'Email Accounts',
 							'open' 	=> 1,
 							'items'	=> $items);
 
-// aufräumen
-unset($items);
+//**** Spamfilter menu
+$items = array();
 
-/*
-	Spamfilter menu
-*/
-
-$items[] = array( 'title' 	=> "Whitelist",
+$items[] = array( 'title' 	=> 'Whitelist',
 				  'target' 	=> 'content',
 				  'link'	=> 'mail/spamfilter_whitelist_list.php');
 
-$items[] = array( 'title' 	=> "Blacklist",
+$items[] = array( 'title' 	=> 'Blacklist',
 				  'target' 	=> 'content',
 				  'link'	=> 'mail/spamfilter_blacklist_list.php');
 
-if($_SESSION["s"]["user"]["typ"] == 'admin') {
+if($_SESSION['s']['user']['typ'] == 'admin') {
 
-	$items[] = array( 	'title' 	=> "User / Domain",
+	$items[] = array( 	'title' 	=> 'User / Domain',
 				  		'target' 	=> 'content',
-				  		'link'	=> 'mail/spamfilter_users_list.php');
+				  		'link'	    => 'mail/spamfilter_users_list.php');
 
-	$items[] = array( 	'title' 	=> "Policy",
+	$items[] = array( 	'title' 	=> 'Policy',
 				  		'target' 	=> 'content',
-				  		'link'	=> 'mail/spamfilter_policy_list.php');
+				  		'link'	    => 'mail/spamfilter_policy_list.php');
 }
 
-$module["nav"][] = array(	'title'	=> 'Spamfilter',
+$module['nav'][] = array(	'title'	=> 'Spamfilter',
 							'open' 	=> 1,
 							'items'	=> $items);
 
-// aufräumen
-unset($items);
+//**** Fetchmail menu
+$items = array();
 
-/*
-	Fetchmail menu
-*/
-
-
-$items[] = array( 'title' 	=> "Fetchmail",
+$items[] = array( 'title' 	=> 'Fetchmail',
 				  'target' 	=> 'content',
 				  'link'	=> 'mail/mail_get_list.php');
 
 
-$module["nav"][] = array(	'title'	=> 'Fetchmail',
+$module['nav'][] = array(	'title'	=> 'Fetchmail',
 							'open' 	=> 1,
 							'items'	=> $items);
 
-// aufräumen
-unset($items);
 
-/*
-	Global filters menu
-*/
+//**** Global filters menu
+$items = array();
+if($_SESSION['s']['user']['typ'] == 'admin') {
 
-if($_SESSION["s"]["user"]["typ"] == 'admin') {
+	$items[] = array(   'title' 	=> 'Postfix Whitelist',
+  				  	    'target' 	=> 'content',
+				  	    'link'	    => 'mail/mail_whitelist_list.php');
 
-	$items[] = array( 'title' 	=> "Postfix Whitelist",
-				  	'target' 	=> 'content',
-				  	'link'	=> 'mail/mail_whitelist_list.php');
-
-	$items[] = array( 'title' 	=> "Postfix Blacklist",
-				 	 'target' 	=> 'content',
-				  	 'link'	=> 'mail/mail_blacklist_list.php');
+	$items[] = array(   'title' 	=> 'Postfix Blacklist',
+				 	    'target' 	=> 'content',
+				  	    'link'	    => 'mail/mail_blacklist_list.php');
 	
-	$items[] = array( 'title' 	=> "Content Filter",
-				 	 'target' 	=> 'content',
-				  	 'link'	=> 'mail/mail_content_filter_list.php');
+	$items[] = array(   'title' 	=> 'Content Filter',
+				 	    'target' 	=> 'content',
+				  	    'link'	    => 'mail/mail_content_filter_list.php');
 
-	$module["nav"][] = array(	'title'	=> 'Global Filters',
+	$module['nav'][] = array(	'title'	=> 'Global Filters',
 								'open' 	=> 1,
 								'items'	=> $items);
-
-	// aufräumen
-	unset($items);
 }
-
 
 ?>
