0a436ac13760be8c882bda66984c7e7ef009798c
oliver <oliver@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2007-08-29 15:30:37 +0000

    1.) Added final_*_destiny settings in amavisd_user_config.master to enable quarantining if wanted. 2.) Cleaned up update.php 3.) Changed definitions for quarantine settings 4.) Commented out areas for quarantining 'clean' and 'other'
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@236 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- install/update.php
+++ install/update.php
@@ -28,18 +28,17 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-/*
-	ISPConfig 3 installer.
-*/
+
+//** ISPConfig 3 installer.
 
 
-// Include the library with the basic installer functions
+//** Include the library with the basic installer functions
 require_once('lib/install.lib.php');
 
-// Include the base class of the installer class
+//** Include the base class of the installer class
 require_once('lib/installer_base.lib.php');
 
-//** Install logfile
+//** Installer/updater logfile
 define('ISPC_LOG_FILE', '/var/log/ispconfig_install.log');
 
 //** Get distribution identifier
@@ -49,12 +48,11 @@ include_once("/usr/local/ispconfig/server/lib/config.inc.php");
 $conf_old = $conf;
 unset($conf);
 
-// Include the distribution specific installer class library
-// and configuration
+//** Include the distribution specific installer class library and configuration
 include_once('dist/lib/'.$distname.'.lib.php');
 include_once('dist/conf/'.$distname.'.conf.php');
 
-// Set the mysql login information
+//** Set the mysql login information
 $conf["mysql"]["host"] = $conf_old["db_host"];
 $conf["mysql"]["database"] = $conf_old["db_database"];
 $conf["mysql"]["ispconfig_user"] = $conf_old["db_user"];
@@ -62,24 +60,22 @@ $conf["mysql"]["ispconfig_password"] = $conf_old["db_password"];
 
 $inst = new installer();
 
-
 echo "This application will update ISPConfig 3 on your server.\n";
 
-// $conf["language"] = $inst->request_language();
+//** TODO: all other queries, for testing I will setup everything in $conf
 
-// TODO: all other queries, for testing I will setup everything in $conf
-
-// Initialize the MySQL server connection
+//** Initialize the MySQL server connection
 include_once('lib/mysql.lib.php');
 
-// Database update is a bit brute force and should be rebuild later ;)
+//** Database update is a bit brute force and should be rebuild later ;)
 
-// export the current database data
+//** Ask user for mysql admin_password if empty
 if( empty($conf["mysql"]["admin_password"]) ) {
 
 	$conf["mysql"]["admin_password"] = $inst->free_query('MySQL root password', $conf['mysql']['admin_password']);
 }
 
+//** export the current database data
 if( !empty($conf["mysql"]["admin_password"]) ) {
 
 	system("mysqldump -h ".$conf['mysql']['host']." -u ".$conf['mysql']['admin_user']." -p".$conf['mysql']['admin_password']." -c -t --add-drop-table --all --quick ".$conf['mysql']['database']." > existing_db.sql");
@@ -89,7 +85,7 @@ else {
 	system("mysqldump -h ".$conf['mysql']['host']." -u ".$conf['mysql']['admin_user']." -c -t --add-drop-table --all --quick ".$conf['mysql']['database']." > existing_db.sql");
 }
 
-// Delete the old database
+//** Delete the old database
 $inst->db = new db();
 
 if( !$inst->db->query('DROP DATABASE IF EXISTS '.$conf['mysql']['database']) ) {
@@ -97,81 +93,74 @@ if( !$inst->db->query('DROP DATABASE IF EXISTS '.$conf['mysql']['database']) ) {
 	$inst->error('Unable to drop MySQL database: '.$conf['mysql']['database'].'.');
 }
 
-
-/*exec("/etc/init.d/mysql stop");
-sleep(3);
-if($conf["mysql"]["database"] != '') exec("rm -rf /var/lib/mysql/".$conf["mysql"]["database"]);
-exec("/etc/init.d/mysql start");
-sleep(5);*/
-
-// Create the mysql database
+//** Create the mysql database
 $inst->configure_database();
 
-// empty all databases
+//** empty all databases
 $db_tables = $inst->db->getTables();
+
 foreach($db_tables as $table) {
+
 	$inst->db->query("TRUNCATE $table");
 }
 
-// load old data back into database
-if($conf["mysql"]["admin_password"] != '') {
+//** load old data back into database
+if( !empty($conf["mysql"]["admin_password"]) ) {
+
 	system("mysql -h ".$conf['mysql']['host']." -u ".$conf['mysql']['admin_user']." -p".$conf['mysql']['admin_password']." ".$conf['mysql']['database']." < existing_db.sql");
 } else {
+
 	system("mysql -h ".$conf['mysql']['host']." -u ".$conf['mysql']['admin_user']." ".$conf['mysql']['database']." < existing_db.sql");
 }
-// Configure postfix
+
+//** Configure postfix
 $inst->configure_postfix('dont-create-certs');
 
-// Configure saslauthd
+//** Configure saslauthd
 swriteln('Configuring SASL');
 $inst->configure_saslauthd();
 
-
-// Configure PAM
+//** Configure PAM
 swriteln('Configuring PAM');
 $inst->configure_pam();
 
-// Configure courier
+//** Configure courier
 swriteln('Configuring Courier');
 $inst->configure_courier();
 
-// Configure Spamasassin
+//** Configure Spamasassin
 swriteln('Configuring Spamassassin');
 $inst->configure_spamassassin();
 
-// Configure Amavis
+//** Configure Amavis
 swriteln('Configuring Amavisd');
 $inst->configure_amavis();
 
-// Configure Getmail
+//** Configure Getmail
 swriteln('Configuring Getmail');
 $inst->configure_getmail();
 
-// Configure Pureftpd
+//** Configure Pureftpd
 swriteln('Configuring Pureftpd');
 $inst->configure_pureftpd();
 
-// Configure MyDNS
+//** Configure MyDNS
 swriteln('Configuring MyDNS');
 $inst->configure_mydns();
 
-// Configure Apache
+//** Configure Apache
 swriteln('Configuring Apache');
 $inst->configure_apache();
 
-// Configure ISPConfig
+//** Configure ISPConfig
 swriteln('Installing ISPConfig');
 $inst->install_ispconfig();
 
-// Configure ISPConfig
+//** Configure ISPConfig
 swriteln('Installing Crontab');
 $inst->install_crontab();
 
-
-/*
-Restart services:
-*/
-
+//** Restart services:
 swriteln('Restarting services ...');
 system("/etc/init.d/mysql restart");
 system("/etc/init.d/postfix restart");
@@ -188,6 +177,5 @@ system("/etc/init.d/pure-ftpd-mysql restart");
 system("/etc/init.d/mydns restart");
 
 echo "Update finished.\n";
-
 
 ?>
