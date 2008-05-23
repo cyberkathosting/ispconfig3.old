c9eec472cf4ef5b43f021058207c7d00eed05822
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2008-05-23 15:39:14 +0000

    Fix: Make the temp directory for language files writable for the webserver process.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@309 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- install/lib/installer_base.lib.php
+++ install/lib/installer_base.lib.php
@@ -654,6 +654,9 @@ class installer_base {
 		//* Make the global language file directory group writable
 		exec("chmod -R 770 $install_dir/interface/lib/lang");
 		
+		//* Make the temp directory for language file exports writable
+		exec("chmod -R 770 $install_dir/interface/web/temp");
+		
 		//* Make all interface language file directories group writable
 		$handle = @opendir($install_dir.'/interface/web');
 		while ($file = @readdir ($handle)) { 
