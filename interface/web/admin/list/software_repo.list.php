cec5e6b21b6f27f1ed0c372bb25eeed7eded765c
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2008-09-09 10:36:48 +0000

    Added russian translation.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@442 c1a6f2c7-8504-0410-933a-a4dae0184b0a

---
+++ interface/web/admin/list/software_repo.list.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+	Datatypes:
+	- INTEGER
+	- DOUBLE
+	- CURRENCY
+	- VARCHAR
+	- TEXT
+	- DATE
+*/
+
+
+
+// Name of the list
+$liste["name"] 				= "software_repo";
+
+// Database table
+$liste["table"] 			= "software_repo";
+
+// Index index field of the database table
+$liste["table_idx"]			= "software_repo_id";
+
+// Search Field Prefix
+$liste["search_prefix"] 	= "search_";
+
+// Records per page
+$liste["records_per_page"] 	= 15;
+
+// Script File of the list
+$liste["file"]				= "software_repo_list.php";
+
+// Script file of the edit form
+$liste["edit_file"]			= "software_repo_edit.php";
+
+// Script File of the delete script
+$liste["delete_file"]		= "software_repo_del.php";
+
+// Paging Template
+$liste["paging_tpl"]		= "templates/paging.tpl.htm";
+
+// Enable auth
+$liste["auth"]				= "yes";
+
+
+/*****************************************************
+* Suchfelder
+*****************************************************/
+
+$liste["item"][] = array(	'field'		=> "active",
+							'datatype'	=> "VARCHAR",
+							'formtype'	=> "SELECT",
+							'op'		=> "=",
+							'prefix'	=> "",
+							'suffix'	=> "",
+							'width'		=> "",
+							'value'		=> array('y' => "Yes",'n' => "No"));
+
+$liste["item"][] = array(	'field'		=> "repo_name",
+							'datatype'	=> "VARCHAR",
+							'formtype'	=> "TEXT",
+							'op'		=> "like",
+							'prefix'	=> "%",
+							'suffix'	=> "%",
+							'width'		=> "",
+							'value'		=> "");
+
+
+$liste["item"][] = array(	'field'		=> "repo_url",
+							'datatype'	=> "VARCHAR",
+							'formtype'	=> "TEXT",
+							'op'		=> "like",
+							'prefix'	=> "%",
+							'suffix'	=> "%",
+							'width'		=> "",
+							'value'		=> "");
+
+?>
