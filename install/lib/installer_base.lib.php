cb040799dc42436c3790d03dd7237f548d78b3d1
redray <redray@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2008-12-04 17:12:33 +0000

    corrected the install manula for ubuntu 8.04 lts
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@636 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- install/lib/installer_base.lib.php
+++ install/lib/installer_base.lib.php
@@ -292,8 +292,8 @@ class installer_base {
 			'broken_sasl_auth_clients = yes',
 			'smtpd_recipient_restrictions = permit_mynetworks, permit_sasl_authenticated, check_recipient_access mysql:'.$config_dir.'/mysql-virtual_recipient.cf, reject_unauth_destination',
 			'smtpd_use_tls = yes',
-			'smtpd_tls_cert_file = '.$config_dir.'/smtpd.cert',
-			'smtpd_tls_key_file = '.$config_dir.'/smtpd.key',
+			'smtpd_tls_cert_file = '.$config_dir.'/ssl/smtpd.cert',
+			'smtpd_tls_key_file = '.$config_dir.'/ssl/smtpd.key',
 			'transport_maps = proxy:mysql:'.$config_dir.'/mysql-virtual_transports.cf',
 			'relay_domains = mysql:'.$config_dir.'/mysql-virtual_relaydomains.cf',
 			'virtual_create_maildirsize = yes',
@@ -332,12 +332,13 @@ class installer_base {
 		
 		if(!stristr($options,'dont-create-certs')) {
 			//* Create the SSL certificate
-			$command = 'cd '.$config_dir.'; '
+			$command = 'mkdir '.$config_dir.'/ssl; '
+                      .'cd '.$config_dir.'/ssl; '
                       .'openssl req -new -outform PEM -out smtpd.cert -newkey rsa:2048 -nodes -keyout '
                       .'smtpd.key -keyform PEM -days 365 -x509';
 			exec($command);
 		
-			$command = 'chmod o= '.$config_dir.'/smtpd.key';
+			$command = 'chmod o= '.$config_dir.'/ssl/smtpd.key';
 			caselog($command.' &> /dev/null', __FILE__, __LINE__, 'EXECUTED: '.$command, 'Failed to execute the command '.$command);
 		}
 		
