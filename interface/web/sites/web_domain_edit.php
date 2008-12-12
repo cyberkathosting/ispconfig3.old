02bac7b92c8f4ff193133432d90554d2b1327670
vogelor <vogelor@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2008-12-12 16:33:24 +0000

    fixed a bug in disallow changing server
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@704 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/sites/web_domain_edit.php
+++ interface/web/sites/web_domain_edit.php
@@ -251,13 +251,15 @@ class page_action extends tform_actions {
 		//* Check if the server has been changed
 		// We do this only for the admin or reseller users, as normal clients can not change the server ID anyway
 		if($_SESSION["s"]["user"]["typ"] == 'admin' || $app->auth->has_clients($_SESSION['s']['user']['userid'])) {
-			$rec = $app->db->queryOneRecord("SELECT server_id from web_domain WHERE domain_id = ".$this->id);
-			if($rec['server_id'] != $this->dataRecord["server_id"]) {
-				//* Add a error message and switch back to old server
-				$app->tform->errorMessage .= $app->lng('The Server can not be changed.');
-				$this->dataRecord["server_id"] = $rec['server_id'];
+			if (isset($this->dataRecord["server_id"])){
+				$rec = $app->db->queryOneRecord("SELECT server_id from web_domain WHERE domain_id = ".$this->id);
+				if($rec['server_id'] != $this->dataRecord["server_id"]) {
+					//* Add a error message and switch back to old server
+					$app->tform->errorMessage .= $app->lng('The Server can not be changed.');
+					$this->dataRecord["server_id"] = $rec['server_id'];
+				}
+				unset($rec);
 			}
-			unset($rec);
 		}
 		
 		//* Check that all fields for the SSL cert creation are filled
