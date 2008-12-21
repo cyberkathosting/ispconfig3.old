735a3a4e280061e0bef98d5499e6e69dab1fa602
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2008-12-21 10:28:22 +0000

    Changes in the client form that modify sys_user were not logged to the datalog correctly.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@769 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/client/client_edit.php
+++ interface/web/client/client_edit.php
@@ -135,18 +135,15 @@ class page_action extends tform_actions {
 		if($this->oldDataRecord['username'] != $this->dataRecord['username']) {
 			$username = mysql_real_escape_string($this->dataRecord["username"]);
 			$client_id = $this->id;
-			$sql = "UPDATE sys_user SET username = '$username' WHERE client_id = $client_id";
-			$app->db->query($sql);
-			$sql = "UPDATE sys_group SET name = '$username' WHERE client_id = $client_id";
-			$app->db->query($sql);
+			$app->db->datalogUpdate('sys_user', "username = '$username'", 'client_id', $client_id);
+			$app->db->datalogUpdate('sys_group', "name = '$username'", 'client_id', $client_id);
 		}
 		
 		// password changed
 		if(isset($this->dataRecord["password"]) && $this->dataRecord["password"] != '') {
 			$password = mysql_real_escape_string($this->dataRecord["password"]);
 			$client_id = $this->id;
-			$sql = "UPDATE sys_user SET passwort = md5('$password') WHERE client_id = $client_id";
-			$app->db->query($sql);
+			$app->db->datalogUpdate('sys_user', "passwort = md5('$password')", 'client_id', $client_id);
 		}
 		
 		// reseller status changed
@@ -155,8 +152,7 @@ class page_action extends tform_actions {
 			if($this->dataRecord["limit_client"] > 0) $modules .= ',client';
 			$modules = mysql_real_escape_string($modules);
 			$client_id = $this->id;
-			$sql = "UPDATE sys_user SET modules = '$modules' WHERE client_id = $client_id";
-			$app->db->query($sql);
+			$app->db->datalogUpdate('sys_user', "modules = '$modules'", 'client_id', $client_id);
 		}
 		/*
 		 *  If there is a client-template, process it */
