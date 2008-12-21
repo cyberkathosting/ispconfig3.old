dcf9f6fc34ada5861debc38fcec58a502d5f5e35
fantu <fantu@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2008-12-21 14:16:15 +0000

    small xhtml fix
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@784 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/lib/app.inc.php
+++ interface/lib/app.inc.php
@@ -114,11 +114,13 @@ class app {
 		//$this->uses("error");
 		//$this->error->message($msg, $priority);
 		if($stop == true){
-			$msg = '<html>
+			$msg = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <title>Error</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link href="../themes/default/style.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="../themes/default/style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <br /><br /><br />
