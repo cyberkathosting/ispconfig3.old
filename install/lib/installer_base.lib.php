82d41848e8fd9791336083766d0ba1fd1fc0d8d3
vogelor <vogelor@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2009-01-12 19:05:01 +0000

    little flaw in configure maildrop
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@866 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- install/lib/installer_base.lib.php
+++ install/lib/installer_base.lib.php
@@ -429,8 +429,8 @@ class installer_base {
         }
 		$configfile = $config_dir.'/master.cf';
 		$content = rf($configfile);
-		$content = str_replace('  flags=DRhu user=vmail argv=/usr/bin/maildrop -d ${recipient}', 
-                   '  flags=R user='.$cf['vmail_username'].' argv=/usr/bin/maildrop -d ${recipient} ${extension} ${recipient} ${user} ${nexthop} ${sender}',
+		$content = str_replace('flags=DRhu user=vmail argv=/usr/bin/maildrop -d ${recipient}', 
+                   'flags=R user='.$cf['vmail_username'].' argv=/usr/bin/maildrop -d ${recipient} ${extension} ${recipient} ${user} ${nexthop} ${sender}',
                      $content);
 		wf($configfile, $content);
 		
