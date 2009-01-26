6215d3ab3c0a7f0f6dc1e41b8ef5e7d391d8519f
vogelor <vogelor@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2009-01-26 19:06:59 +0000

    Fixed problem with suexec, cgi and openbasedir
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@894 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- server/plugins-available/apache2_plugin.inc.php
+++ server/plugins-available/apache2_plugin.inc.php
@@ -569,7 +569,7 @@ class apache2_plugin {
 			$cgi_tpl = new tpl();
 			$cgi_tpl->newTemplate("php-cgi-starter.master");
 
-			$cgi_tpl->setVar('open_basedir',$data["new"]["document_root"]);
+			$cgi_tpl->setVar('open_basedir', "/var/www/" . $data["new"]["domain"]);
 			$cgi_tpl->setVar('php_cgi_bin',$cgi_config["cgi_bin"]);
 
 			$cgi_starter_script = escapeshellcmd($cgi_starter_path.$cgi_config["cgi_starter_script"]);
