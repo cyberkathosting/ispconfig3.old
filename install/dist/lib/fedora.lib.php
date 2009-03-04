a8a37bb3b86a09128d75f75327083b4887bd115e
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2009-03-04 15:10:04 +0000

    Fixed path in suphp.conf for fedora.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@990 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- install/dist/lib/fedora.lib.php
+++ install/dist/lib/fedora.lib.php
@@ -406,7 +406,7 @@ class installer_dist extends installer_base {
 		
 		if(is_file('/etc/suphp.conf')) {
 			replaceLine('/etc/suphp.conf','php=php:/usr/bin','x-httpd-suphp=php:/usr/bin/php-cgi',0);
-			replaceLine('/etc/suphp.conf','docroot=','docroot=/var/clients',0);
+			replaceLine('/etc/suphp.conf','docroot=','docroot=/var/www',0);
 		}
 		
 		//* Create the logging directory for the vhost logfiles
