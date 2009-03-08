2f3fd387879ef0009fe8d24665aa1d22ecff27a3
redray <redray@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2009-03-08 10:02:12 +0000

    fixed myDns database bug http://www.howtoforge.com/forums/showthread.php?t=32237
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1029 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- install/sql/ispconfig3.sql
+++ install/sql/ispconfig3.sql
@@ -161,7 +161,7 @@ CREATE TABLE `dns_rr` (
   `zone` int(11) unsigned NOT NULL,
   `name` varchar(64) NOT NULL,
   `type` enum('A','AAAA','ALIAS','CNAME','HINFO','MX','NAPTR','NS','PTR','RP','SRV','TXT') default NULL,
-  `data` varchar(128) NOT NULL,
+  `data` varchar(255) NOT NULL,
   `aux` int(11) unsigned NOT NULL default '0',
   `ttl` int(11) unsigned NOT NULL default '86400',
   `active` enum('N','Y') NOT NULL default 'Y',
