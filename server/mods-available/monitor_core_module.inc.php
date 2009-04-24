d3c16be576a4065bd2dd8e9181b5fbc51788c666
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2009-04-24 13:28:31 +0000

    Exclude cramfs shares from disk monitoring.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1180 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- server/mods-available/monitor_core_module.inc.php
+++ server/mods-available/monitor_core_module.inc.php
@@ -221,7 +221,7 @@ class monitor_core_module {
                 $usePercent = floatval($data[$i]['percent']);
 				
 				//* We dont want to check the cdrom drive as a cd / dvd is always 100% full
-				if($data[$i]['type'] != 'iso9660') {
+				if($data[$i]['type'] != 'iso9660' && $data[$i]['type'] != 'cramfs') {
                 	if ($usePercent > 75) $state = $this->_setState($state, 'info');
                 	if ($usePercent > 80) $state = $this->_setState($state, 'warning');
                 	if ($usePercent > 90) $state = $this->_setState($state, 'critical');
