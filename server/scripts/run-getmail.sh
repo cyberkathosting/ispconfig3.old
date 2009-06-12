713c44ce8293d7ce7c9fa95878e23081af132b6f
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2009-06-12 11:04:35 +0000

    Fixed getmail error in log when no getmail accounts had been created.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1228 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- server/scripts/run-getmail.sh
+++ server/scripts/run-getmail.sh
@@ -3,7 +3,11 @@ set -e
 cd /etc/getmail
 rcfiles=""
 for file in *.conf ; do
+if [ $file != "*.conf" ]; then
 rcfiles="$rcfiles -r $file"
+fi
 done
 #echo $rcfiles
+if [ "$rcfiles" != "" ]; then
 exec /usr/bin/getmail -n -v -g /etc/getmail $rcfiles
+fi
