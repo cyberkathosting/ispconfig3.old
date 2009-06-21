ff1d891bb7860583396f75b1943ad74b3f3f60e0
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2009-06-21 14:39:22 +0000

    Fixed: FS#778 - mailalias has priority over mailbox if they both exist. Added a error message that prevents it to add a alias or forward when a mailbox with the same email address exists.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1234 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/mail/mail_forward_edit.php
+++ interface/web/mail/mail_forward_edit.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Copyright (c) 2005, Till Brehm, projektfarm Gmbh
+Copyright (c) 2005 - 2009, Till Brehm, projektfarm Gmbh
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
@@ -122,6 +122,11 @@ class page_action extends tform_actions {
 		
 		unset($this->dataRecord["email_local_part"]);
 		unset($this->dataRecord["email_domain"]);
+		
+		//* Check if there is no mailbox with this address
+		$tmp = $app->db->queryOneRecord("SELECT count(mailuser_id) as number FROM mail_user WHERE email = '".$app->db->quote($this->dataRecord["source"])."'");
+		if($tmp['number'] > 0) $app->tform->errorMessage .= $app->tform->lng("duplicate_mailbox_txt")."<br>";
+		unset($tmp);
 		
 		parent::onSubmit();
 	}
