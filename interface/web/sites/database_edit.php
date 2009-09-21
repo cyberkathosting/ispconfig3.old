e1bbcf7eff73f08861f1ea1149732aec1feddbf1
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2009-09-21 15:55:47 +0000

    Fixed: FS#879 - Reseller cannot create database for himself, only for "c0" or subclients.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1379 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/sites/database_edit.php
+++ interface/web/sites/database_edit.php
@@ -85,7 +85,7 @@ class page_action extends tform_actions {
 
 			// Get the limits of the client
 			$client_group_id = $_SESSION["s"]["user"]["default_group"];
-			$client = $app->db->queryOneRecord("SELECT client.client_id, limit_web_domain, default_webserver FROM sys_group, client WHERE sys_group.client_id = client.client_id and sys_group.groupid = $client_group_id");
+			$client = $app->db->queryOneRecord("SELECT client.client_id, limit_web_domain, default_webserver, contact_name FROM sys_group, client WHERE sys_group.client_id = client.client_id and sys_group.groupid = $client_group_id");
 			
 			// Set the webserver to the default server of the client
 			$tmp = $app->db->queryOneRecord("SELECT server_name FROM server WHERE server_id = $client[default_webserver]");
@@ -95,7 +95,7 @@ class page_action extends tform_actions {
 			// Fill the client select field
 			$sql = "SELECT groupid, name FROM sys_group, client WHERE sys_group.client_id = client.client_id AND client.parent_client_id = ".$client['client_id'];
 			$clients = $app->db->queryAllRecords($sql);
-			$client_select = '';
+			$client_select = '<option value="'.$client['client_id'].'">'.$client['contact_name'].'</option>';
 			if(is_array($clients)) {
 				foreach( $clients as $client) {
 					$selected = @($client["groupid"] == $this->dataRecord["sys_groupid"])?'SELECTED':'';
