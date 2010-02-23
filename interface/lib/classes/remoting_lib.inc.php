984a44e91bb45f3d0ebf7a6c98b32981f2b4389a
moglia <moglia@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2010-02-23 22:08:46 +0000

    Modified loadUserProfile function i discovered using mailuser interface from horfic after many tests and debugs functions recive a value iguals to userid field not sys_userid. I keep sysuser_id testing for compatibility issues no have any idea if this used in another place and added aditional userid test. Tested all things and used auto-response with roundcube with this modifications all worked fine. More info see here: http://www.howtoforge.com/forums/showthread.php?t=38437&page=15
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1602 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/lib/classes/remoting_lib.inc.php
+++ interface/lib/classes/remoting_lib.inc.php
@@ -149,11 +149,14 @@ class remoting_lib {
 				$this->sys_default_group     = 1;
 				$this->sys_groups            = 1;
 			} else {
-				//* load system user
+				//* load system user - try with sysuser and before with userid (workarrond)
 				$user = $app->db->queryOneRecord("SELECT * FROM sys_user WHERE sysuser_id = $client_id");
 				if(empty($user["userid"])) {
-					$this->errorMessage .= 'No sysuser with the ID $client_id found.';
-					return false;
+						$user = $app->db->queryOneRecord("SELECT * FROM sys_user WHERE userid = $client_id");		
+						if(empty($user["userid"])) {
+								$this->errorMessage .= "No sysuser with the ID $client_id found.";
+								return false;
+						}
 				}
 				$this->sys_username         = $user['username'];
 				$this->sys_userid            = $user['userid'];
@@ -162,7 +165,7 @@ class remoting_lib {
 			}
 
 		return true;
-	}  
+	    }  
 
 
         /**
