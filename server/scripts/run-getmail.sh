6cae194164c07c8f6ce5e5b71f7ad9fead4a2f62
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2010-02-25 14:48:26 +0000

    Fixed: FS#1060 - multiple execution of Fetchmail / Getmail causes high load
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1611 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- server/scripts/run-getmail.sh
+++ server/scripts/run-getmail.sh
@@ -8,6 +8,12 @@ rcfiles="$rcfiles -r $file"
 fi
 done
 #echo $rcfiles
-if [ "$rcfiles" != "" ]; then
-exec /usr/bin/getmail -n -v -g /etc/getmail $rcfiles
+if [ -f /tmp/.getmail_lock ]; then
+  echo 'Found getmail lock file /tmp/.getmail_lock, we quit here.'
+else
+  touch /tmp/.getmail_lock
+  if [ "$rcfiles" != "" ]; then
+    exec /usr/bin/getmail -n -v -g /etc/getmail $rcfiles
+  fi
+  rm -f /tmp/.getmail_lock
 fi
