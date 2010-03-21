06fd14ffaf11ba23ead11a31af8b3b348ca013bd
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2010-03-21 10:45:06 +0000

    Fixed: FS#1116 - Cron error
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1660 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- server/cron_daily.php
+++ server/cron_daily.php
@@ -186,9 +186,9 @@ if(is_array($records)) {
 	foreach($records as $rec){
 		$tmp_path = realpath(escapeshellcmd($rec["document_root"].'/tmp'));
 		if($tmp_path != '' && strlen($tmp_path) > 10 && is_dir($tmp_path) && $app->system->is_user($rec['system_user'])){
-			exec("cd ".$tmp_path."; find -ctime +1 -user ".escapeshellcmd($rec['system_user'])." | grep -v -w .no_delete | xargs rm &> /dev/null");
-			if($app->system->is_user('www-data')) exec("cd ".$tmp_path."; find -ctime +1 -user www-data | grep -v -w .no_delete | xargs rm &> /dev/null");
-			if($app->system->is_user('wwwrun')) exec("cd ".$tmp_path."; find -ctime +1 -user wwwrun | grep -v -w .no_delete | xargs rm &> /dev/null");
+			exec("cd ".$tmp_path."; find -ctime +1 -user ".escapeshellcmd($rec['system_user'])." | grep -v -w .no_delete | xargs rm &> /dev/null 2> /dev/null");
+			if($app->system->is_user('www-data')) exec("cd ".$tmp_path."; find -ctime +1 -user www-data | grep -v -w .no_delete | xargs rm &> /dev/null 2> /dev/null");
+			if($app->system->is_user('wwwrun')) exec("cd ".$tmp_path."; find -ctime +1 -user wwwrun | grep -v -w .no_delete | xargs rm &> /dev/null 2> /dev/null");
 		}
 	}
 }
