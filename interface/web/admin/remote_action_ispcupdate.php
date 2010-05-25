91d7ca741e5f696f2b9a37e368346e6e14415f0f
vogelor <vogelor@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2010-05-25 15:12:11 +0000

    fixed a typo in remote-actions
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1749 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/admin/remote_action_ispcupdate.php
+++ interface/web/admin/remote_action_ispcupdate.php
@@ -74,7 +74,7 @@ if (isset($_POST['server_select'])) {
 		$servers[] = $_POST['server_select'];
 	}
 	foreach ($servers as $serverId) {
-		$sql = "INSERT INTO sys_remoteaction (server_id, tstamp, action_type, action_param, action_status, response) " .
+		$sql = "INSERT INTO sys_remoteaction (server_id, tstamp, action_type, action_param, action_state, response) " .
 				"VALUES (".
 				(int)$serverId . ", " .
 				time() . ", " .
