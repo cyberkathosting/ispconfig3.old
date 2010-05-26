8d7c805bebd6cbb5807aa4661e3ac44799163793
vogelor <vogelor@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2010-05-26 09:44:37 +0000

    Enhancement of the Email-Domain-List. The user can only select the server as filter, he has some mail-domains at (and not ALL Servers of the server-farm)
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1754 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/mail/list/mail_domain.list.php
+++ interface/web/mail/list/mail_domain.list.php
@@ -66,7 +66,7 @@ $liste["item"][] = array(	'field'		=> "server_id",
 							'prefix'	=> "%",
 							'suffix'	=> "%",
 							'datasource'	=> array ( 	'type'	=> 'SQL',
-														'querystring' => 'SELECT server_id,server_name FROM server WHERE {AUTHSQL} ORDER BY server_name',
+														'querystring' => 'SELECT a.server_id, a.server_name FROM server a, mail_domain b WHERE (a.server_id = b.server_id) AND ({AUTHSQL-B}) ORDER BY a.server_name',
 														'keyfield'=> 'server_id',
 														'valuefield'=> 'server_name'
 									 				  ),
