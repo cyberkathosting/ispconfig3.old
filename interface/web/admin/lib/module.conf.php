0903298e81d7ed7c39c16e9f2d7b929ce321035e
mike_p <mike_p@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2010-05-29 19:01:11 +0000

    Create ids for the html so that css styling can be used
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1793 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/admin/lib/module.conf.php
+++ interface/web/admin/lib/module.conf.php
@@ -11,13 +11,15 @@ $module['tab_width']    = '60';
 
 $items[] = array( 'title' 	=> 'Add user',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/users_edit.php');
+				  'link'	=> 'admin/users_edit.php',
+				  'html_id'=> 'user_add');
 
 $items[] = array( 'title' 	=> 'Edit user',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/users_list.php');
+				  'link'	=> 'admin/users_list.php',
+				  'html_id'=> 'user_list');
 
-				  
+
 $module['nav'][] = array(	'title'	=> 'CP Users',
 							'open' 	=> 1,
 							'items'	=> $items);
@@ -28,13 +30,15 @@ unset($items);
 /*
 $items[] = array( 'title' 	=> 'Add group',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/groups_edit.php');
+				  'link'	=> 'admin/groups_edit.php',
+				  'html_id'=> 'group_add');
 
 $items[] = array( 'title' 	=> 'Edit group',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/groups_list.php');
+				  'link'	=> 'admin/groups_list.php',
+				  'html_id'=> 'group_list');
 
-				  
+
 $module['nav'][] = array(	'title'	=> 'Groups',
 							'open' 	=> 1,
 							'items'	=> $items);
@@ -46,24 +50,29 @@ unset($items);
 /*
 $items[] = array( 'title' 	=> 'Add server',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/server_edit.php');
+				  'link'	=> 'admin/server_edit.php',
+				  'html_id'=> 'server_add');
 */
 $items[] = array( 'title' 	=> 'Server Services',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/server_list.php');
+				  'link'	=> 'admin/server_list.php',
+				  'html_id'=> 'server_list');
 
 $items[] = array( 'title' 	=> 'Server Config',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/server_config_list.php');
+				  'link'	=> 'admin/server_config_list.php',
+				  'html_id'=> 'server_config_list');
 
 /*
 $items[] = array( 'title' 	=> 'Add Server IP',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/server_ip_edit.php');
+				  'link'	=> 'admin/server_ip_edit.php',
+				  'html_id'=> 'server_ip_edit');
 */
 $items[] = array( 'title' 	=> 'Edit Server IP',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/server_ip_list.php');				  
+				  'link'	=> 'admin/server_ip_list.php',
+				  'html_id'=> 'server_ip_list');
 
 
 $items[] = array( 'title' 	=> 'Interface Config',
@@ -79,7 +88,8 @@ unset($items);
 
 $items[] = array( 'title' 	=> 'Firewall',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/firewall_list.php');				  
+				  'link'	=> 'admin/firewall_list.php',
+				  'html_id'=> 'firewall_list');
 
 
 $module['nav'][] = array(	'title'	=> 'Firewall',
@@ -93,15 +103,18 @@ unset($items);
 
 $items[] = array( 'title' 	=> 'Repositories',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/software_repo_list.php');
+				  'link'	=> 'admin/software_repo_list.php',
+				  'html_id'=> 'software_repo_list');
 
 $items[] = array( 'title' 	=> 'Packages',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/software_package_list.php');
+				  'link'	=> 'admin/software_package_list.php',
+				  'html_id'=> 'software_package_list');
 
 $items[] = array( 'title' 	=> 'Updates',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/software_update_list.php');
+				  'link'	=> 'admin/software_update_list.php',
+				  'html_id'=> 'software_update_list');
 
 $module['nav'][] = array(	'title'	=> 'Software',
 							'open' 	=> 1,
@@ -113,23 +126,28 @@ unset($items);
 
 $items[] = array( 'title' 	=> 'Languages',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/language_list.php');
+				  'link'	=> 'admin/language_list.php',
+				  'html_id'=> 'language_list');
 
 $items[] = array( 'title' 	=> 'New Language',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/language_add.php');
-				  
+				  'link'	=> 'admin/language_add.php',
+				  'html_id'=> 'language_add');
+
 $items[] = array( 'title' 	=> 'Merge',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/language_complete.php');
+				  'link'	=> 'admin/language_complete.php',
+				  'html_id'=> 'language_complete');
 
 $items[] = array( 'title' 	=> 'Export',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/language_export.php');
+				  'link'	=> 'admin/language_export.php',
+				  'html_id'=> 'language_export');
 
 $items[] = array( 'title' 	=> 'Import',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/language_import.php');
+				  'link'	=> 'admin/language_import.php',
+				  'html_id'=> 'language_import');
 
 $module['nav'][] = array(	'title'	=> 'Language Editor',
 							'open' 	=> 1,
@@ -141,13 +159,15 @@ unset($items);
 
 $items[] = array( 'title' 	=> 'Add user',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/remote_user_edit.php');
+				  'link'	=> 'admin/remote_user_edit.php',
+				  'html_id'=> 'remote_user_add');
 
 $items[] = array( 'title' 	=> 'Edit user',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/remote_user_list.php');
+				  'link'	=> 'admin/remote_user_list.php',
+				  'html_id'=> 'remote_user_list');
 
-				  
+
 $module['nav'][] = array(	'title'	=> 'Remote Users',
 							'open' 	=> 1,
 							'items'	=> $items);
@@ -157,11 +177,13 @@ unset($items);
 
 $items[] = array( 'title' 	=> 'Do OS-Update',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/remote_action_osupdate.php');
+				  'link'	=> 'admin/remote_action_osupdate.php',
+				  'html_id'=> 'osupdate');
 
 $items[] = array( 'title' 	=> 'Do ISPConfig-Update',
 				  'target' 	=> 'content',
-				  'link'	=> 'admin/remote_action_ispcupdate.php');
+				  'link'	=> 'admin/remote_action_ispcupdate.php',
+				  'html_id'=> 'ispcupdate');
 
 $module['nav'][] = array(	'title'	=> 'Remote Actions',
 							'open' 	=> 1,
