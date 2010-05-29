432e4f8147be53d1967b437704d71ca06010d32e
mike_p <mike_p@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2010-05-29 20:25:17 +0000

    Create ids for the html so that css styling can be used
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1798 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/client/lib/module.conf.php
+++ interface/web/client/lib/module.conf.php
@@ -13,16 +13,19 @@ $module["tab_width"]    = '';
 
 $items[] = array( 'title' 	=> "Add Client",
 				  'target' 	=> 'content',
-				  'link'	=> 'client/client_edit.php');
+				  'link'	=> 'client/client_edit.php',
+				  'html_id' => 'client_add');
 
 $items[] = array( 'title' 	=> "Edit Client",
 				  'target' 	=> 'content',
-				  'link'	=> 'client/client_list.php');
+				  'link'	=> 'client/client_list.php',
+				  'html_id' => 'client_list');
 
 if($_SESSION["s"]["user"]["typ"] == 'admin'){
 	$items[] = array( 'title' 	=> "Edit Client-Templates",
 					  'target' 	=> 'content',
-					  'link'	=> 'client/client_template_list.php');
+					  'link'	=> 'client/client_template_list.php',
+					  'html_id' => 'client_template_list');
 }
 
 $module["nav"][] = array(	'title'	=> 'Clients',
@@ -36,26 +39,18 @@ if($_SESSION["s"]["user"]["typ"] == 'admin'){
 
 $items[] = array( 'title' 	=> "Add Reseller",
 				  'target' 	=> 'content',
-				  'link'	=> 'client/reseller_edit.php');
+				  'link'	=> 'client/reseller_edit.php',
+				  'html_id' => 'reseller_add');
 
 $items[] = array( 'title' 	=> "Edit Reseller",
 				  'target' 	=> 'content',
-				  'link'	=> 'client/reseller_list.php');
+				  'link'	=> 'client/reseller_list.php',
+				  'html_id' => 'reseller_list');
 
 $module["nav"][] = array(	'title'	=> 'Resellers',
 							'open' 	=> 1,
 							'items'	=> $items);
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
