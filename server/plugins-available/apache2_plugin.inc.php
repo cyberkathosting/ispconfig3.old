d7a9625152c30dc148855b273acf461e76f881dd
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2010-06-02 13:08:33 +0000

    Fixed: FS#1129 - Domain redirection
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/branches/ispconfig-3.0.2@1812 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- server/plugins-available/apache2_plugin.inc.php
+++ server/plugins-available/apache2_plugin.inc.php
@@ -582,9 +582,11 @@ class apache2_plugin {
 		$rewrite_rules = array();
 		if($data["new"]["redirect_type"] != '') {
 			if(substr($data["new"]["redirect_path"],-1) != '/') $data["new"]["redirect_path"] .= '/';
+			/* Disabled path extension
 			if($data["new"]["redirect_type"] == 'no' && substr($data["new"]["redirect_path"],0,4) != 'http') {
 				$data["new"]["redirect_path"] = $data["new"]["document_root"]."/web".realpath($data["new"]["redirect_path"]).'/';
 			}
+			*/
 			
 			$rewrite_rules[] = array(	'rewrite_domain' 	=> $data["new"]["domain"],
 										'rewrite_type' 		=> ($data["new"]["redirect_type"] == 'no')?'':'['.$data["new"]["redirect_type"].']',
@@ -633,9 +635,11 @@ class apache2_plugin {
 				// Rewriting
 				if($alias["redirect_type"] != '') {
 					if(substr($data["new"]["redirect_path"],-1) != '/') $data["new"]["redirect_path"] .= '/';
+					/* Disabled path extension
 					if($data["new"]["redirect_type"] == 'no' && substr($data["new"]["redirect_path"],0,4) != 'http') {
 						$data["new"]["redirect_path"] = $data["new"]["document_root"]."/web".realpath($data["new"]["redirect_path"]).'/';
 					}
+					*/
 					$rewrite_rules[] = array(	'rewrite_domain' 	=> $alias["domain"],
 												'rewrite_type' 		=> ($alias["redirect_type"] == 'no')?'':'['.$alias["redirect_type"].']',
 												'rewrite_target' 	=> $alias["redirect_path"]);
