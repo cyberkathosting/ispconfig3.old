d70e4312adda44a97a0e76416c402968c324343f
jmontoya <jmontoya@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2010-09-02 15:32:25 +0000

    Adding trim in the ini_parser::get_ini_string() function
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@1952 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/lib/classes/ini_parser.inc.php
+++ interface/lib/classes/ini_parser.inc.php
@@ -59,6 +59,8 @@ class ini_parser{
 			$content .= "[$section]\n";
 			foreach($data as $item => $value) {
 				if($item != ''){
+					$value 	= trim($value);
+					$item 	= trim($item);
                     $content .= "$item=$value\n";
                 }
 			}
