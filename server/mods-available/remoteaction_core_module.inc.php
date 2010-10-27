cbf3f9fd4ec3968584537c90be1f91fb88762411
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2010-10-27 11:49:33 +0000

    Fixed: FS#1391 - SVN Update failed (remoteaction_core_module.inc.php)
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@2118 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- server/mods-available/remoteaction_core_module.inc.php
+++ server/mods-available/remoteaction_core_module.inc.php
@@ -27,7 +27,7 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-class remoteaction_core_module extends modules_base {
+class remoteaction_core_module {
 	var $module_name = 'remoteaction_core_module';
 	var $class_name = 'remoteaction_core_module';
 	/* No actions at this time. maybe later... */
