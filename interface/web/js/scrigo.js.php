d262df885c745623347fb70ed9f4485dc21a97a8
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2011-02-22 15:31:50 +0000

    Fixed: FS#1521 - Script error on login page when using IE8 Switch to IE8 mode when IE9 is used.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/branches/ispconfig-3.0.3@2203 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/js/scrigo.js.php
+++ interface/web/js/scrigo.js.php
@@ -503,3 +503,14 @@ function delAdditionalTemplate(){
   }
   
 }
+
+function getInternetExplorerVersion() {
+    var rv = -1; // Return value assumes failure.
+    if (navigator.appName == 'Microsoft Internet Explorer') {
+        var ua = navigator.userAgent;
+        var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+            rv = parseFloat(RegExp.$1);
+    }
+    return rv;
+}
