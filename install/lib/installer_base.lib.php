92c123c05e44efc808d14ae6254ffec0f401073b
laking <laking@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2011-04-02 21:48:43 +0000

    Update to set CA-path and CA-pass ny the installer.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@2258 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- install/lib/installer_base.lib.php
+++ install/lib/installer_base.lib.php
@@ -1355,6 +1355,10 @@ class installer_base {
 		$content = str_replace('{ispconfig_log_priority}', $conf['ispconfig_log_priority'], $content);
 		$content = str_replace('{language}', $conf['language'], $content);
 
+		if (!$conf['CA-enabled']) $content = str_replace('$conf[\'CA', '//$conf[\'CA', $content);
+		$content = str_replace('{CA-path}', $conf['CA-path'], $content);
+		$content = str_replace('{CA-pass}', $conf['CA-pass'], $content);
+
 		wf($install_dir.'/server/lib/'.$configfile, $content);
 
 		//* Create the config file for remote-actions (but only, if it does not exist, because
