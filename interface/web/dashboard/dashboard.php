d5872e1d487a9a633313a59527dbbef9b5314f6f
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2011-04-04 15:01:19 +0000

    Extended dashboard plugins.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/branches/ispconfig-3.0.3@2269 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/dashboard/dashboard.php
+++ interface/web/dashboard/dashboard.php
@@ -126,7 +126,7 @@ while ($file = @readdir ($handle)) {
 
 /* Which dashlets in which column */
 /******************************************************************************/
-$leftcol_dashlets = array('modules');
+$leftcol_dashlets = array('modules','invoices');
 $rightcol_dashlets = array('limits');
 /******************************************************************************/
 
