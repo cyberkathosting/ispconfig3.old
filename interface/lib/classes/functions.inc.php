290bcec00ba790f5103537d9770a8c2f5adcee8c
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2011-04-05 13:40:38 +0000

    Fixed bug in functions library.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/branches/ispconfig-3.0.3@2280 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/lib/classes/functions.inc.php
+++ interface/lib/classes/functions.inc.php
@@ -98,7 +98,7 @@ class functions {
 	
 	public function get_ispconfig_url() {
 		$url = (stristr($_SERVER['SERVER_PROTOCOL'],'HTTPS'))?'https':'http';
-		$url .= '//'.$_SERVER['SERVER_NAME'];
+		$url .= '://'.$_SERVER['SERVER_NAME'];
 		if($_SERVER['SERVER_PORT'] != 80 && $_SERVER['SERVER_PORT'] != 443) {
 			$url .= ':'.$_SERVER['SERVER_PORT'];
 		}
