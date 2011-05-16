49a0b9f414e462962cf56061607facedacd7a11b
latham <latham@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2011-05-16 13:37:07 +0000

    Typo, its Debian 6
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@2340 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- install/dist/conf/debian60.conf.php
+++ install/dist/conf/debian60.conf.php
@@ -28,7 +28,7 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-//***  Debian 4.0 default settings
+//***  Debian 6.0 default settings
 
 //* Main
 $conf['language'] = 'en';
