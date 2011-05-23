2ed01abbb8e1db8a248c1b53b086817ba7c64c84
latham <latham@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2011-05-23 21:26:10 +0000

    comment by line so the user can test
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@2361 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/lib/config.inc.local.php
+++ interface/lib/config.inc.local.php
@@ -1,25 +1,29 @@
 <?php
 
 /*
+
 Example local config.  Here you can add your own custom
 settings to override the defaults. These will also be
 protected from updates.
 
-$conf['app_title'] = "My Web Hosting";
-$conf['app_version'] = "2.0";
-$conf['app_link'] = 'http://www.mycompany.com';
-$conf['modules_available'] = 'dashboard,admin,mail,sites,monitor,client,dns,help';
+*/
 
-$conf['interface_modules_enabled'] = 'dashboard,mail,sites,dns,tools';
 
-$conf['software_updates_enabled'] = false;
+// $conf['app_title'] = "My Web Hosting";
+// $conf['app_version'] = "2.0";
+// $conf['app_link'] = 'http://www.mycompany.com';
+// $conf['modules_available'] = 'dashboard,admin,mail,sites,monitor,client,dns,help';
 
-$conf['theme'] = 'mycustom_theme'; // Must be installed.
-$conf['logo'] = 'themes/default/images/ispc_logo.png';
+// $conf['interface_modules_enabled'] = 'dashboard,mail,sites,dns,tools';
 
-$conf['language'] = 'en';
+// $conf['software_updates_enabled'] = false;
 
-$conf['interface_logout_url'] = 'http://www.mycompany.com';
+// $conf['theme'] = 'mycustom_theme'; // Must be installed.
+// $conf['logo'] = 'themes/default/images/ispc_logo.png';
+
+// $conf['language'] = 'en';
+
+// $conf['interface_logout_url'] = 'http://www.mycompany.com';
 */
 
 ?>
