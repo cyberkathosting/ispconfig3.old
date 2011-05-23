b94d6c3941edfb64c526d3e816ce3f24ce08cad9
latham <latham@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2011-05-23 22:28:01 +0000

    Small fixes..
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@2364 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/help/lib/admin.conf.php
+++ interface/web/help/lib/admin.conf.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-	The admin.conf.php file contains menu definitions to be displayed in the administration modile.
+	The admin.conf.php file contains menu definitions to be displayed in the administration module.
 */
 
 
