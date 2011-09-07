11f8e212886a0b82ba191cc9f3634776664c80d3
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2011-09-07 13:33:17 +0000

    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@2491 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- server/plugins-available/cron_jailkit_plugin.inc.php
+++ server/plugins-available/cron_jailkit_plugin.inc.php
@@ -291,8 +291,8 @@ class cron_jailkit_plugin {
 			$this->app->log("Added jailkit user to chroot with command: ".$command,LOGLEVEL_DEBUG);
 				
 			mkdir(escapeshellcmd($this->parent_domain['document_root'].$jailkit_chroot_userhome), 0755, true);
-			chown(escapeshellcmd($this->parent_domain['document_root'].$jailkit_chroot_userhome, escapeshellcmd($this->parent_domain['system_user']));
-			chgrp(escapeshellcmd($this->parent_domain['document_root'].$jailkit_chroot_userhome, escapeshellcmd($this->parent_domain['system_group']));
+			chown(escapeshellcmd($this->parent_domain['document_root'].$jailkit_chroot_userhome), escapeshellcmd($this->parent_domain['system_user']));
+			chgrp(escapeshellcmd($this->parent_domain['document_root'].$jailkit_chroot_userhome), escapeshellcmd($this->parent_domain['system_group']));
 			
 	}
 	
