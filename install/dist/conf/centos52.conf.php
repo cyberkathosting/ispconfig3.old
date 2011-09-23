18bfb3e2e08f1ac0080d94a09fceb62edb5e88e4
falko <falko@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2011-09-23 09:25:30 +0000

    Changed PHP-FPM socket directory from /var/run/php5-fpm to /var/lib/php5-fpm because /var/run is cleand up on reboot.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@2611 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- install/dist/conf/centos52.conf.php
+++ install/dist/conf/centos52.conf.php
@@ -192,7 +192,7 @@ $conf['nginx']['php_fpm_init_script'] = 'php-fpm';
 $conf['nginx']['php_fpm_ini_path'] = '/etc/php.ini';
 $conf['nginx']['php_fpm_pool_dir'] = '/etc/php-fpm.d';
 $conf['nginx']['php_fpm_start_port'] = 9010;
-$conf['nginx']['php_fpm_socket_dir'] = '/var/run/php5-fpm';
+$conf['nginx']['php_fpm_socket_dir'] = '/var/lib/php5-fpm';
 
 //* vlogger
 $conf['vlogger']['config_dir'] = '/etc';
--- server/scripts/create_daily_nginx_access_logs.sh
+++ server/scripts/create_daily_nginx_access_logs.sh
@@ -6,4 +6,4 @@ do
   mv $f/access.log $f/`date "+%Y%m%d"`-access.log &> /dev/null
   touch $f/access.log &> /dev/null
 done
-/etc/init.d/nginx reload  &> /dev/null
+pkill -USR1 -u root nginx &> /dev/null
