20784c71a3bcb081db189f3c03689b4fca975bfc
falko <falko@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-01-09 11:00:11 +0000

    - nginx: Fixed bug in time calculation for yesterday's access log that caused Webalizer/AWstats to show a wrong date.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/branches/ispconfig-3.0.4@2875 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- server/scripts/create_daily_nginx_access_logs.sh
+++ server/scripts/create_daily_nginx_access_logs.sh
@@ -3,7 +3,7 @@ PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/X11R6/bin
 FILES=/var/log/ispconfig/httpd/*
 for f in $FILES
 do
-  mv $f/access.log $f/`date "+%Y%m%d"`-access.log &> /dev/null
+  mv $f/access.log $f/`date --date='yesterday' "+%Y%m%d"`-access.log &> /dev/null
   touch $f/access.log &> /dev/null
 done
 pkill -USR1 -u root nginx &> /dev/null
