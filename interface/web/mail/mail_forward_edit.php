dc367ba512bf7c24ac756533fe4bf91668af248d
falko <falko@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-04-10 10:07:07 +0000

    - Fixed FS#2074.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/branches/ispconfig-3.0.4@3038 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/mail/mail_forward_edit.php
+++ interface/web/mail/mail_forward_edit.php
@@ -74,7 +74,8 @@ class page_action extends tform_actions {
 		$app->tpl->setVar("email_local_part",$email_parts[0]);
 		
 		// Getting Domains of the user
-		$sql = "SELECT domain FROM mail_domain WHERE ".$app->tform->getAuthSQL('r').' ORDER BY domain';
+		//$sql = "SELECT domain FROM mail_domain WHERE ".$app->tform->getAuthSQL('r').' ORDER BY domain';
+		$sql = "SELECT domain FROM mail_domain WHERE domain NOT IN (SELECT SUBSTR(source,2) FROM mail_forwarding WHERE type = 'aliasdomain') AND ".$app->tform->getAuthSQL('r')." ORDER BY domain";
 		$domains = $app->db->queryAllRecords($sql);
 		$domain_select = '';
 		foreach( $domains as $domain) {
