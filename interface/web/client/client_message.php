9d8ce145a9887e8329da79c6138c440bd647b2a6
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-04-19 10:28:23 +0000

    Implemented: FS#2085 - Add a function to send emails to customers
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/branches/ispconfig-3.0.4@3053 c1a6f2c7-8504-0410-933a-a4dae0184b0a

---
+++ interface/web/client/client_message.php
@@ -0,0 +1,110 @@
+<?php
+/*
+Copyright (c) 2012, Till Brehm, ISPConfig UG
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of ISPConfig nor the names of its contributors
+      may be used to endorse or promote products derived from this software without
+      specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+require_once('../../lib/config.inc.php');
+require_once('../../lib/app.inc.php');
+
+//* Check permissions for module
+$app->auth->check_module_permissions('client');
+
+//* This function is not available in demo mode
+if($conf['demo_mode'] == true) $app->error('This function is disabled in demo mode.');
+
+$app->uses('tpl');
+
+$app->tpl->newTemplate('form.tpl.htm');
+$app->tpl->setInclude('content_tpl', 'templates/client_message.htm');
+
+//* load language file 
+$lng_file = 'lib/lang/'.$_SESSION['s']['language'].'_client_message.lng';
+include($lng_file);
+$app->tpl->setVar($wb);
+
+$msg = '';
+$error = '';
+
+//* Save data
+if(isset($_POST) && count($_POST) > 1) {
+	
+	//* Check values
+	if(!preg_match("/^\w+[\w\.\-\+]*\w{0,}@\w+[\w.-]*\w+\.[a-zA-Z0-9\-]{2,30}$/i", $_POST['sender'])) $error .= $wb['sender_invalid_error'].'<br />';
+	if(empty($_POST['subject'])) $error .= $wb['subject_invalid_error'].'<br />';
+	if(empty($_POST['message'])) $error .= $wb['message_invalid_error'].'<br />';
+	
+	//* Send message
+	if($error == '') {
+		//* Select all clients and resellers
+		if($_SESSION["s"]["user"]["typ"] == 'admin'){
+			$sql = "SELECT * FROM client WHERE email != ''";
+		} else {
+			$client_id = intval($_SESSION['s']['user']['client_id']);
+			if($client_id == 0) die('Invalid Client ID.');
+			$sql = "SELECT * FROM client WHERE email != '' AND parent_client_id = '$client_id'";
+		}
+		
+		//* Get clients
+		$clients = $app->db->queryAllRecords($sql);
+		if(is_array($clients)) {
+			$msg = $wb['email_sent_to_txt'].' ';
+			foreach($clients as $client) {
+				
+				//* Parse cleint details into message
+				$message = $_POST['message'];
+				foreach($client as $key => $val) {
+					$message = str_replace('{'.$key.'}', $val, $message);
+				}
+				
+				//* Send the email
+				$app->functions->mail($client['email'], $_POST['subject'], $message, $_POST['sender']);
+				$msg .= $client['email'].', ';
+			}
+			$msg = substr($msg,0,-2);
+		}
+		
+	} else {
+		$app->tpl->setVar('sender',$_POST['sender']);
+		$app->tpl->setVar('subject',$_POST['subject']);
+		$app->tpl->setVar('message',$_POST['message']);
+	}
+}
+
+if($_SESSION["s"]["user"]["typ"] == 'admin'){
+	$app->tpl->setVar('form_legend_txt',$wb['form_legend_admin_txt']);
+} else {
+	$app->tpl->setVar('form_legend_txt',$wb['form_legend_client_txt']);
+}
+
+$app->tpl->setVar('okmsg',$msg);
+$app->tpl->setVar('error',$error);
+
+$app->tpl_defaults();
+$app->tpl->pparse();
+
+
+?>
