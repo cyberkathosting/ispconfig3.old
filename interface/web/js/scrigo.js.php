50dfe91591d3f4019b934eb6390973e6a1f45d94
falko <falko@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-05-16 16:42:15 +0000

    - Added JavaScript functions for generating passwords. - Added "Generate Password" links to forms of the sites module.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3111 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/js/scrigo.js.php
+++ interface/web/js/scrigo.js.php
@@ -525,3 +525,33 @@ function getInternetExplorerVersion() {
     }
     return rv;
 }
+
+function password(length, special) {
+	var iteration = 0;
+	var password = "";
+	var randomNumber;
+	length = length || 10;
+	if(special == undefined){
+		var special = false;
+	}
+	while(iteration < length){
+		randomNumber = (Math.floor((Math.random() * 100)) % 94) + 33;
+		if(!special){
+			if ((randomNumber >=33) && (randomNumber <=47)) { continue; }
+			if ((randomNumber >=58) && (randomNumber <=64)) { continue; }
+			if ((randomNumber >=91) && (randomNumber <=96)) { continue; }
+			if ((randomNumber >=123) && (randomNumber <=126)) { continue; }
+		}
+		iteration++;
+		password += String.fromCharCode(randomNumber);
+	}
+	return password;
+}
+
+function generatePassword(passwordFieldID){
+	var oldPWField = jQuery('#'+passwordFieldID);
+	var newPWField = oldPWField.clone();
+	newPWField.attr('type', 'text').attr('id', 'tmp'+passwordFieldID).insertBefore(oldPWField);
+	oldPWField.remove();
+	newPWField.attr('id', passwordFieldID).val(password(10, true)).trigger('keyup');
+}
