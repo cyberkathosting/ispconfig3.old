71e6df976cac4c4e2f4a777cdc38ef769fd404ce
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-06-07 10:59:07 +0000

    FS#1814 - Add option for custom php.ini for execution of ispconfig server.php script
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3183 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- server/cron_daily.sh
+++ server/cron_daily.sh
@@ -2,4 +2,11 @@
 
 PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/X11R6/bin
 
+if [ -f /usr/local/ispconfig/server/lib/php.ini ]; then
+        PHPINIOWNER=`stat -c %U /usr/local/ispconfig/server/lib/php.ini`
+        if [ $PHPINIOWNER == 'root' ] || [ $PHPINIOWNER == 'ispconfig'  ]; then
+                export PHPRC=/usr/local/ispconfig/server/lib
+        fi
+fi
+
 /usr/bin/php -q /usr/local/ispconfig/server/cron_daily.php
--- server/server.sh
+++ server/server.sh
@@ -4,5 +4,12 @@ PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/X11R6/bin
 
 . /etc/profile
 
+if [ -f /usr/local/ispconfig/server/lib/php.ini ]; then
+        PHPINIOWNER=`stat -c %U /usr/local/ispconfig/server/lib/php.ini`
+        if [ $PHPINIOWNER == 'root' ] || [ $PHPINIOWNER == 'ispconfig'  ]; then
+                export PHPRC=/usr/local/ispconfig/server/lib
+        fi
+fi
+
 cd /usr/local/ispconfig/server
 /usr/bin/php -q /usr/local/ispconfig/server/server.php
