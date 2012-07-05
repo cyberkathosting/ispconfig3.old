9bc46eccf332e097e7c6910b6207caad409d9339
cfoe <cfoe@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-07-05 13:48:56 +0000

    added overview to subNav + changed <label> w/o INPUT to <p>s
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3246 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/mailuser/lib/module.conf.php
+++ interface/web/mailuser/lib/module.conf.php
@@ -12,43 +12,41 @@ $module['tab_width']            = '';
 //**** menu
 $items = array();
 
-$items[] = array( 	'title' 	=> 'Overview',
-					'target' 	=> 'content',
-					'link'	=> 'mailuser/index.php',
-					'html_id' => 'mail_user_index');
+$items[] = array( 	'title'     => 'Overview',
+                        'target'    => 'content',
+                        'link'      => 'mailuser/index.php',
+                        'html_id'   => 'mail_user_index');
 
-$items[] = array( 	'title' 	=> 'Password',
-					'target' 	=> 'content',
-					'link'	=> 'mailuser/mail_user_password_edit.php',
-					'html_id' => 'mail_user_password');
+$items[] = array( 	'title'     => 'Password',
+                        'target'    => 'content',
+                        'link'      => 'mailuser/mail_user_password_edit.php',
+                        'html_id'   => 'mail_user_password');
 
-$items[] = array( 	'title' 	=> 'Autoresponder',
-					'target' 	=> 'content',
-					'link'	=> 'mailuser/mail_user_autoresponder_edit.php',
-					'html_id' => 'mail_user_autoresponder');
+$items[] = array( 	'title'     => 'Autoresponder',
+                        'target'    => 'content',
+                        'link'      => 'mailuser/mail_user_autoresponder_edit.php',
+                        'html_id'   => 'mail_user_autoresponder');
 
-$items[] = array( 	'title' 	=> 'Send copy',
-					'target' 	=> 'content',
-					'link'	=> 'mailuser/mail_user_cc_edit.php',
-					'html_id' => 'mail_user_cc');
+$items[] = array( 	'title'     => 'Send copy',
+                        'target'    => 'content',
+                        'link'      => 'mailuser/mail_user_cc_edit.php',
+                        'html_id'   => 'mail_user_cc');
 
-$items[] = array( 	'title' 	=> 'Spamfilter',
-					'target' 	=> 'content',
-					'link'	=> 'mailuser/mail_user_spamfilter_edit.php',
-					'html_id' => 'mail_user_cc');
+$items[] = array( 	'title'     => 'Spamfilter',
+                        'target'    => 'content',
+                        'link'      => 'mailuser/mail_user_spamfilter_edit.php',
+                        'html_id'   => 'mail_user_cc');
 
-$items[] = array( 	'title' 	=> 'Email Filters',
-					'target' 	=> 'content',
-					'link'	=> 'mailuser/mail_user_filter_list.php',
-					'html_id' => 'mail_user_filter_list');
+$items[] = array( 	'title'     => 'Email Filters',
+                        'target'    => 'content',
+                        'link'      => 'mailuser/mail_user_filter_list.php',
+                        'html_id'   => 'mail_user_filter_list');
 
 
 if(count($items)) {
-	$module['nav'][] = array(	'title'	=> 'Email Account',
-								'open' 	=> 1,
-								'items'	=> $items);
+	$module['nav'][] = array(   'title' => 'Email Account',
+                                    'open'  => 1,
+                                    'items' => $items);
 }
-
-
 
 ?>
