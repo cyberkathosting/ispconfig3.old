846049d2e2946d03354d3363d57cdc315e5ca525
cfoe <cfoe@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-07-14 13:33:24 +0000

    changed mail_user_list autoresponder filter INPUT to SELECT
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3276 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/mail/list/mail_user.list.php
+++ interface/web/mail/list/mail_user.list.php
@@ -13,13 +13,13 @@
 
 
 // Name of the list
-$liste["name"] 				= "mail_user";
+$liste["name"] 			= "mail_user";
 
 // Database table
-$liste["table"] 			= "mail_user";
+$liste["table"] 		= "mail_user";
 
 // Index index field of the database table
-$liste["table_idx"]			= "mailuser_id";
+$liste["table_idx"]		= "mailuser_id";
 
 // Search Field Prefix
 $liste["search_prefix"] 	= "search_";
@@ -28,10 +28,10 @@ $liste["search_prefix"] 	= "search_";
 $liste["records_per_page"] 	= "15";
 
 // Script File of the list
-$liste["file"]				= "mail_user_list.php";
+$liste["file"]			= "mail_user_list.php";
 
 // Script file of the edit form
-$liste["edit_file"]			= "mail_user_edit.php";
+$liste["edit_file"]		= "mail_user_edit.php";
 
 // Script File of the delete script
 $liste["delete_file"]		= "mail_user_del.php";
@@ -40,48 +40,47 @@ $liste["delete_file"]		= "mail_user_del.php";
 $liste["paging_tpl"]		= "templates/paging.tpl.htm";
 
 // Enable auth
-$liste["auth"]				= "yes";
+$liste["auth"]			= "yes";
 
 
 /*****************************************************
 * Suchfelder
 *****************************************************/
 
-$liste["item"][] = array( 'field'   => "email",
-              'datatype'  => "VARCHAR",
-              'formtype'  => "TEXT",
-              'op'    => "like",
-              'prefix'  => "%",
-              'suffix'  => "%",
-              'width'   => "",
-              'value'   => "");
+$liste["item"][] = array(   'field'     => "email",
+                            'datatype'  => "VARCHAR",
+                            'formtype'  => "TEXT",
+                            'op'        => "like",
+                            'prefix'    => "%",
+                            'suffix'    => "%",
+                            'width'     => "",
+                            'value'     => "");
 
-$liste["item"][] = array( 'field'   => "login",
-              'datatype'  => "VARCHAR",
-              'formtype'  => "TEXT",
-              'op'    => "like",
-              'prefix'  => "%",
-              'suffix'  => "%",
-              'width'   => "",
-              'value'   => "");
+$liste["item"][] = array(   'field'     => "login",
+                            'datatype'  => "VARCHAR",
+                            'formtype'  => "TEXT",
+                            'op'        => "like",
+                            'prefix'    => "%",
+                            'suffix'    => "%",
+                            'width'     => "",
+                            'value'     => "");
 
-$liste["item"][] = array(	'field'		=> "name",
-							'datatype'	=> "VARCHAR",
-							'formtype'	=> "TEXT",
-							'op'		=> "like",
-							'prefix'	=> "%",
-							'suffix'	=> "%",
-							'width'		=> "",
-							'value'		=> "");
+$liste["item"][] = array(   'field'     => "name",
+                            'datatype'	=> "VARCHAR",
+                            'formtype'	=> "TEXT",
+                            'op'	=> "like",
+                            'prefix'	=> "%",
+                            'suffix'	=> "%",
+                            'width'	=> "",
+                            'value'	=> "");
 
-$liste["item"][] = array(	'field'		=> "autoresponder",
-							'datatype'	=> "VARCHAR",
-							'formtype'	=> "TEXT",
-							'op'		=> "like",
-							'prefix'	=> "%",
-							'suffix'	=> "%",
-							'width'		=> "",
-							'value'		=> "");
-
-
+$liste["item"][] = array(   'field'     => "autoresponder",
+                            'datatype'	=> "VARCHAR",
+                            'formtype'	=> "SELECT",
+                            'op'	=> "=",
+                            'prefix'	=> "",
+                            'suffix'	=> "",
+                            'width'	=> "",
+                            'value'	=> array('y' => "<div id=\"ir-Yes\" class=\"swap\"><span>Yes</span></div>", 'n' => "<div class=\"swap\" id=\"ir-No\"><span>No</span></div>"));							
+							
 ?>
