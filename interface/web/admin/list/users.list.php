458f28673958b0e1f70f45c84102f7096e300687
cfoe <cfoe@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-07-17 08:25:02 +0000

    added groups to users_list (TODO: name resolution for group_ids)
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3283 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/admin/list/users.list.php
+++ interface/web/admin/list/users.list.php
@@ -10,13 +10,13 @@
 */
 
 //* Name of the list
-$liste['name'] 				= 'users';
+$liste['name']                  = 'users';
 
 //* Database table
-$liste['table'] 			= 'sys_user';
+$liste['table'] 		= 'sys_user';
 
 //* Primary index column
-$liste['table_idx']			= 'userid';
+$liste['table_idx']		= 'userid';
 
 //* Search column prefix
 $liste['search_prefix'] 	= 'search_';
@@ -25,10 +25,10 @@ $liste['search_prefix'] 	= 'search_';
 $liste['records_per_page'] 	= "15";
 
 //* Script file for list
-$liste['file']				= 'users_list.php';
+$liste['file']			= 'users_list.php';
 
 //* Script file to edit
-$liste['edit_file']			= 'users_edit.php';
+$liste['edit_file']		= 'users_edit.php';
 
 //* Script file to delete
 $liste['delete_file']		= 'users_del.php';
@@ -37,37 +37,43 @@ $liste['delete_file']		= 'users_del.php';
 $liste['paging_tpl']		= 'templates/paging.tpl.htm';
 
 //* Enable auth
-$liste['auth']				= 'no';
+$liste['auth']			= 'no';
 
 
 /*****************************************************
 * Suchfelder
 *****************************************************/
 
-$liste["item"][] = array(	'field'		=> "client_id",
-							'datatype'	=> "INTEGER",
-							'formtype'	=> "TEXT",
-							'op'		=> "=",
-							'prefix'	=> "",
-							'suffix'	=> "",
-							'width'		=> "",
-							'value'		=> "");
+$liste["item"][] = array(   'field'     => "client_id",
+                            'datatype'	=> "INTEGER",
+                            'formtype'	=> "TEXT",
+                            'op'	=> "=",
+                            'prefix'	=> "",
+                            'suffix'	=> "",
+                            'width'	=> "",
+                            'value'	=> "");
 
-$liste["item"][] = array(	'field'		=> "active",
-							'datatype'	=> "VARCHAR",
-							'formtype'	=> "SELECT",
-							'op'		=> "=",
-							'prefix'	=> "",
-							'suffix'	=> "",
-							'width'		=> "",
-							'value'		=> array('1' => "<div id=\"ir-Yes\" class=\"swap\"><span>Yes</span></div>",'0' => "<div class=\"swap\" id=\"ir-No\"><span>No</span></div>"));
+$liste["item"][] = array(   'field'	=> "active",
+                            'datatype'	=> "VARCHAR",
+                            'formtype'	=> "SELECT",
+                            'op'	=> "=",
+                            'prefix'	=> "",
+                            'suffix'	=> "",
+                            'width'	=> "",
+                            'value'	=> array('1' => "<div id=\"ir-Yes\" class=\"swap\"><span>Yes</span></div>",'0' => "<div class=\"swap\" id=\"ir-No\"><span>No</span></div>"));
 
+$liste['item'][] = array(   'field'	=> 'username',
+                            'datatype'	=> 'VARCHAR',
+                            'op'	=> 'like',
+                            'prefix'	=> '%',
+                            'suffix'	=> '%',
+                            'width'	=> '');
 
-$liste['item'][] = array(	'field'		=> 'username',
-							'datatype'	=> 'VARCHAR',
-							'op'		=> 'like',
-							'prefix'	=> '%',
-							'suffix'	=> '%',
-							'width'		=> '');
+$liste['item'][] = array(   'field'	=> 'groups',
+                            'datatype'	=> 'VARCHAR',
+                            'op'	=> 'like',
+                            'prefix'	=> '%',
+                            'suffix'	=> '%',
+                            'width'	=> '');
 
 ?>
