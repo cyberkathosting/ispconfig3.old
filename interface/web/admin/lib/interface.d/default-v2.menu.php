9d196bdcea0debe7f3a833b38a4bd75a469a7199
cfoe <cfoe@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-07-18 17:44:18 +0000

    theme config menu file
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3293 c1a6f2c7-8504-0410-933a-a4dae0184b0a

---
+++ interface/web/admin/lib/interface.d/default-v2.menu.php
@@ -0,0 +1,7 @@
+<?php
+
+$items[] = array(   'title'     => 'Default-v2 Theme',
+                    'target' 	=> 'content',
+                    'link'	=> 'admin/default-v2_settings.php',
+                    'html_id'   => 'default-v2_settings');
+?>
