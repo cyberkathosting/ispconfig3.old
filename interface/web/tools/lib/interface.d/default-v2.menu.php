43307e90d3899bd5a560e8187b41b2137bd84de3
cfoe <cfoe@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-07-18 17:45:49 +0000

    added interface + theme options for the end user
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3294 c1a6f2c7-8504-0410-933a-a4dae0184b0a

---
+++ interface/web/tools/lib/interface.d/default-v2.menu.php
@@ -0,0 +1,7 @@
+<?php
+
+$items[] = array(   'title'     => 'Default-v2 Theme',
+                    'target' 	=> 'content',
+                    'link'	=> 'tools/default-v2_settings.php',
+                    'html_id'   => 'default-v2_settings');
+?>
--- interface/web/tools/lib/module.conf.php
+++ interface/web/tools/lib/module.conf.php
@@ -2,8 +2,8 @@
 
 global $conf;
 
-$module['name'] 		= 'tools';
-$module['title'] 		= 'top_menu_tools';
+$module['name'] 	= 'tools';
+$module['title'] 	= 'top_menu_tools';
 $module['template'] 	= 'module.tpl.htm';
 $module['startpage'] 	= 'tools/index.php';
 $module['tab_width']    = '60';
@@ -12,28 +12,46 @@ $module['tab_width']    = '60';
 //**** Change User password
 $items = array();
 
-$items[] = array( 'title' 	=> 'Password and Language',
-				  'target' 	=> 'content',
-				  'link'	=> 'tools/user_settings.php',
-				  'html_id'=> 'user_settings');
+$items[] = array(   'title' 	=> 'Password and Language',
+                    'target' 	=> 'content',
+                    'link'	=> 'tools/user_settings.php',
+                    'html_id'   => 'user_settings');
 
 
-$module['nav'][] = array(	'title'	=> 'User Settings',
-							'open' 	=> 1,
-							'items'	=> $items);
+$module['nav'][] = array(   'title' => 'User Settings',
+                            'open'  => 1,
+                            'items' => $items);
 
+unset($items);
 
+//**** Change interface settings + load settings page of the activated theme
+$items = array();
+
+$items[] = array(   'title'     => 'Interface',
+                    'target' 	=> 'content',
+                    'link'	=> 'tools/interface_settings.php',
+                    'html_id'   => 'interface_settings');
+
+include_once(ISPC_WEB_PATH.'/tools/lib/interface.d/' . $_SESSION['s']['user']['app_theme'] . '.menu.php');
+				  
+$module['nav'][] = array(	'title'	=> 'Interface',
+                                'open' 	=> 1,
+                                'items'	=> $items);
+
+unset($items);
+
+//**** Load additional menu items
 $menu_dir = ISPC_WEB_PATH.'/tools/lib/menu.d';
 
 if (is_dir($menu_dir)) {
-	if ($dh = opendir($menu_dir)) {
-		//** Go through all files in the menu dir
-		while (($file = readdir($dh)) !== false) {
-			if($file != '.' && $file != '..' && substr($file,-9,9) == '.menu.php' && $file != 'dns_resync.menu.php') {
-				include_once($menu_dir.'/'.$file);
-			}
-		}
-	}
+    if ($dh = opendir($menu_dir)) {
+        //** Go through all files in the menu dir
+        while (($file = readdir($dh)) !== false) {
+            if ($file != '.' && $file != '..' && substr($file, -9, 9) == '.menu.php' && $file != 'dns_resync.menu.php') {
+                include_once($menu_dir . '/' . $file);
+            }
+        }
+    }
 }
 
 ?>
