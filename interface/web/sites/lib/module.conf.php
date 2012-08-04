8742e009512a4e85640386b78fd5a97ec4ee45e9
cfoe <cfoe@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-08-04 21:05:44 +0000

    reduced sites module menu
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3320 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/sites/lib/module.conf.php
+++ interface/web/sites/lib/module.conf.php
@@ -2,207 +2,171 @@
 
 $userid=$app->auth->get_user_id();
 
-$module["name"] 		= "sites";
-$module["title"] 		= "top_menu_sites";
+$module["name"] 	= "sites";
+$module["title"] 	= "top_menu_sites";
 $module["template"] 	= "module.tpl.htm";
 $module["startpage"] 	= "sites/web_domain_list.php";
 $module["tab_width"]    = '';
 
-/*
-	Websites menu
-*/
+// Websites menu
 $items=array();
 
 if($app->auth->get_client_limit($userid,'web_domain') != 0)
 {
-	$items[] = array( 'title' 	=> "Website",
-			'target' 	=> 'content',
-			'link'	=> 'sites/web_domain_list.php',
-			'html_id' => 'domain_list');
+$items[] = array(   'title' 	=> "Website",
+                    'target' 	=> 'content',
+                    'link'	=> 'sites/web_domain_list.php',
+                    'html_id'   => 'domain_list');
 }
 
 if($app->auth->get_client_limit($userid,'web_subdomain') != 0)
 {
-	$items[] = array( 'title' 	=> "Subdomain",
-			'target' 	=> 'content',
-			'link'	=> 'sites/web_subdomain_list.php',
-			'html_id' => 'subdomain_list');
+$items[] = array(   'title' 	=> "Subdomain",
+                    'target' 	=> 'content',
+                    'link'      => 'sites/web_subdomain_list.php',
+                    'html_id'   => 'subdomain_list');
 }
 
 if($app->auth->get_client_limit($userid,'web_aliasdomain') != 0)
 {
-	$items[] = array( 'title'   => "Aliasdomain",
-			'target'  => 'content',
-			'link'    => 'sites/web_aliasdomain_list.php',
-			'html_id' => 'aliasdomain_list');
+$items[] = array(   'title'   => "Aliasdomain",
+                    'target'  => 'content',
+                    'link'    => 'sites/web_aliasdomain_list.php',
+                    'html_id' => 'aliasdomain_list');
 }
 
 if(count($items))
 {
-	$module["nav"][] = array(	'title'	=> 'Websites',
-								'open' 	=> 1,
-								'items'	=> $items);
+$module["nav"][] = array(   'title' => 'Websites',
+                            'open'  => 1,
+                            'items' => $items);
 }
 
-
-/*
-	FTP User menu
-*/
-if($app->auth->get_client_limit($userid,'ftp_user') != 0)
-{
-	$items=array();
-	
-	$items[] = array( 'title' 	=> "FTP-User",
-					  'target' 	=> 'content',
-					  'link'	=> 'sites/ftp_user_list.php',
-					  'html_id' => 'ftp_user_list');
-	
-	
-	$module["nav"][] = array(	'title'	=> 'FTP',
-								'open' 	=> 1,
-								'items'	=> $items);
-}
-
-/*
-	Shell User menu
-*/
-if($app->auth->get_client_limit($userid,'shell_user') != 0)
-{
-	$items=array();
-	
-	$items[] = array( 'title' 	=> "Shell-User",
-					  'target' 	=> 'content',
-					  'link'	=> 'sites/shell_user_list.php',
-					  'html_id' => 'shell_user_list');	
-	
-	$module["nav"][] = array(	'title'	=> 'Shell',
-								'open' 	=> 1,
-								'items'	=> $items);
-}
-
-/*
-	Databases menu
-*/
+// Databases menu
 if($app->auth->get_client_limit($userid,'database') != 0)
 {
-	$items=array();
-	
-	$items[] = array( 'title' 	=> "Database",
-					  'target' 	=> 'content',
-					  'link'	=> 'sites/database_list.php',
-					  'html_id' => 'database_list'
-					  );	
-	
-	$module["nav"][] = array(	'title'	=> 'Database',
-								'open' 	=> 1,
-								'items'	=> $items);
-}
+$items=array();
+
+$items[] = array(   'title'     => "Database",
+                    'target' 	=> 'content',
+                    'link'	=> 'sites/database_list.php',
+                    'html_id'   => 'database_list');
 
 /*
- *	Webdav User menu
- */
+Database User (for future development)
+
+$items[] = array(   'title'     => "Database User",
+                    'target' 	=> 'content',
+                    'link'	=> 'sites/database_user_list.php',
+                    'html_id'   => 'database_user_list'
+);
+*/
+
+$module["nav"][] = array(   'title' => 'Database',
+                            'open'  => 1,
+                            'items' => $items);
+}
+
+// Web menu
+$items=array();
+if($app->auth->get_client_limit($userid,'ftp_user') != 0)
+{
+$items[] = array(   'title'     => "FTP-User",
+                    'target' 	=> 'content',
+                    'link'	=> 'sites/ftp_user_list.php',
+                    'html_id'   => 'ftp_user_list');
+}
+
 if($app->auth->get_client_limit($userid,'webdav_user') != 0)
 {
-	$items=array();
-	
-	$items[] = array( 'title' 	=> "Webdav-User",
-			'target' 	=> 'content',
-			'link'	=> 'sites/webdav_user_list.php',
-			'html_id' => 'webdav_user_list');
-	
-	
-	$module["nav"][] = array(	'title'	=> 'Webdav',
-			'open' 	=> 1,
-			'items'	=> $items);
+$items[] = array(   'title' 	=> "Webdav-User",
+                    'target' 	=> 'content',
+                    'link'	=> 'sites/webdav_user_list.php',
+                    'html_id'   => 'webdav_user_list');
 }
 
-/*
- *	Web folder menu
- */
-	$items=array();
+$items[] = array(   'title'     => "Folder",
+                    'target' 	=> 'content',
+                    'link'	=> 'sites/web_folder_list.php',
+                    'html_id'   => 'web_folder_list');
 	
-	$items[] = array( 'title' 	=> "Folder",
-			'target' 	=> 'content',
-			'link'	=> 'sites/web_folder_list.php',
-			'html_id' => 'web_folder_list');
+$items[] = array(   'title' 	=> "Folder users",
+                    'target' 	=> 'content',
+                    'link'	=> 'sites/web_folder_user_list.php',
+                    'html_id'   => 'web_folder_user_list');
 	
-	$items[] = array( 'title' 	=> "Folder users",
-			'target' 	=> 'content',
-			'link'	=> 'sites/web_folder_user_list.php',
-			'html_id' => 'web_folder_user_list');
-	
-	$module["nav"][] = array(	'title'	=> 'Folder protection',
-			'open' 	=> 1,
-			'items'	=> $items);
+$module["nav"][] = array(   'title' => 'Web Access',
+                            'open'  => 1,
+                            'items' => $items);
 
 
-/*
-    Cron menu
-*/
-if($app->auth->get_client_limit($userid,'cron') != 0)
+// CMD menu
+$items=array();
+
+if($app->auth->get_client_limit($userid,'shell_user') != 0)
 {
-	$items = array();	
-	
-	$items[] = array( 'title'   => "Cron Jobs",
-	                  'target'  => 'content',
-	                  'link'    => 'sites/cron_list.php',
-					  'html_id' => 'cron_list');	
-	
-	$module["nav"][] = array(   'title' => 'Cron',
-	                            'open'  => 1,
-	                            'items' => $items);
+$items[] = array(   'title'     => "Shell-User",
+                    'target' 	=> 'content',
+                    'link'	=> 'sites/shell_user_list.php',
+                    'html_id'   => 'shell_user_list');	
 }
+if($app->auth->get_client_limit($userid,'cron') != 0)
+{	
+$items[] = array(   'title'   => "Cron Jobs",
+                    'target'  => 'content',
+                    'link'    => 'sites/cron_list.php',
+                    'html_id' => 'cron_list');	
+}
+$module["nav"][] = array(   'title' => 'Command Line',
+                            'open'  => 1,
+                            'items' => $items);
 
-//*** APS menu
+
+// APS menu
 $items = array();
 
-$items[] = array('title'   => 'Available packages',
-                 'target'  => 'content',
-                 'link'    => 'sites/aps_availablepackages_list.php',
-                 'html_id' => 'aps_availablepackages_list');
+$items[] = array(   'title'   => 'Available packages',
+                    'target'  => 'content',
+                    'link'    => 'sites/aps_availablepackages_list.php',
+                    'html_id' => 'aps_availablepackages_list');
 
-$items[] = array('title'   => 'Installed packages',
-                 'target'  => 'content',
-                 'link'    => 'sites/aps_installedpackages_list.php',
-                 'html_id' => 'aps_installedpackages_list');
+$items[] = array(   'title'   => 'Installed packages',
+                    'target'  => 'content',
+                    'link'    => 'sites/aps_installedpackages_list.php',
+                    'html_id' => 'aps_installedpackages_list');
                 
 
 // Second menu group, available only for admins
 if($_SESSION['s']['user']['typ'] == 'admin') 
 {
-	$items[] = array('title'   => 'Update Packagelist',
-                 'target'  => 'content',
-                 'link'    => 'sites/aps_cron_apscrawler_if.php',
-                 'html_id' => 'aps_packagedetails_show');
+$items[] = array(   'title'   => 'Update Packagelist',
+                    'target'  => 'content',
+                    'link'    => 'sites/aps_cron_apscrawler_if.php',
+                    'html_id' => 'aps_packagedetails_show');
 }
 
-$module['nav'][] = array('title' => 'APS Installer',
-                         'open'  => 1,
-                         'items' => $items);
+$module['nav'][] = array(   'title' => 'APS Installer',
+                            'open'  => 1,
+                            'items' => $items);
 
 
-//**** Statistics menu
+// Statistics menu
 $items = array();
 
-$items[] = array( 'title'   => 'Web traffic',
-		'target'  => 'content',
-		'link'    => 'sites/web_sites_stats.php',
-		'html_id' => 'websites_stats');
+$items[] = array(   'title'   => 'Web traffic',
+                    'target'  => 'content',
+                    'link'    => 'sites/web_sites_stats.php',
+                    'html_id' => 'websites_stats');
 
-$items[] = array( 'title'   => 'Website quota (Harddisk)',
-		'target'  => 'content',
-		'link'    => 'sites/user_quota_stats.php',
-		'html_id' => 'user_quota_stats');
-
+$items[] = array(   'title'   => 'Website quota (Harddisk)',
+                    'target'  => 'content',
+                    'link'    => 'sites/user_quota_stats.php',
+                    'html_id' => 'user_quota_stats');
 
 $module['nav'][] = array(   'title' => 'Statistics',
-		'open'  => 1,
-		'items' => $items);
-
-
+                            'open'  => 1,
+                            'items' => $items);
 
 // clean up
 unset($items);
-
-
 ?>
