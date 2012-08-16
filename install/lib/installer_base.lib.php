e32506226f73dac48eda132601b40bcf927fd40a
maddinxx <maddinxx@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-08-16 21:03:34 +0000

    better security for auth.log file
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3363 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- install/lib/installer_base.lib.php
+++ install/lib/installer_base.lib.php
@@ -1957,9 +1957,11 @@ class installer_base {
 			touch($conf['ispconfig_log_dir'].'/ispconfig.log');
 		}
 
-		//* Create the ispconfig auth log file
+		//* Create the ispconfig auth log file and set uid/gid
 		if(!is_file($conf['ispconfig_log_dir'].'/auth.log')) {
-			touch($conf['ispconfig_log_dir'].'/auth.log', 0666);
+			touch($conf['ispconfig_log_dir'].'/auth.log');
+			exec('chown ispconfig:ispconfig '. $conf['ispconfig_log_dir'].'/auth.log');
+			exec('chmod 660 '. $conf['ispconfig_log_dir'].'/auth.log');
 		}
 
 		if(is_user('getmail')) {
