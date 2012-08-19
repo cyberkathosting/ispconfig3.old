d27bf3288755437bac0a939aee3ce7dddce478bc
Croydon <Croydon@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-08-19 11:59:17 +0000

    Fixed sql from previous commit
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3378 c1a6f2c7-8504-0410-933a-a4dae0184b0a

---
+++ install/sql/incremental/upd_0037.sql
@@ -0,0 +1,23 @@
+﻿-- --------------------------------------------------------
+
+ALTER TABLE `client` ADD `limit_cgi` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `web_php_options`,
+                     ADD `limit_ssi` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_cgi`,
+                     ADD `limit_perl` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_ssi`,
+                     ADD `limit_ruby` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_perl`,
+                     ADD `limit_python` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_ruby`,
+                     ADD `force_suexec` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'y' AFTER `limit_python`,
+                     ADD `limit_hterror` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `force_suexec`,
+                     ADD `limit_wildcard` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_hterror`,
+                     ADD `limit_ssl` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_wildcard`;
+
+ALTER TABLE `client_template` ADD `limit_cgi` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `web_php_options`,
+                              ADD `limit_ssi` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_cgi`,
+                              ADD `limit_perl` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_ssi`,
+                              ADD `limit_ruby` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_perl`,
+                              ADD `limit_python` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_ruby`,
+                              ADD `force_suexec` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'y' AFTER `limit_python`,
+                              ADD `limit_hterror` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `force_suexec`,
+                              ADD `limit_wildcard` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_hterror`,
+                              ADD `limit_ssl` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_wildcard`;
+
+
