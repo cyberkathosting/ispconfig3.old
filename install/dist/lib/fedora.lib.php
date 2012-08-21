c7274d8dd21e4fa619f905e9272e593989a9ce18
cfoe <cfoe@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-08-21 08:30:24 +0000

    fixed php errors on install/update
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3387 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- install/dist/lib/fedora.lib.php
+++ install/dist/lib/fedora.lib.php
@@ -742,7 +742,7 @@ class installer_dist extends installer_base {
 		$content = str_replace('{ispconfig_log_priority}', $conf['ispconfig_log_priority'], $content);
 		$content = str_replace('{language}', $conf['language'], $content);
 		$content = str_replace('{timezone}', $conf['timezone'], $content);
-		$content = str_replace('{theme}', $conf['theme'], $content)
+		$content = str_replace('{theme}', $conf['theme'], $content);
 		
 		wf("$install_dir/interface/lib/$configfile", $content);
 		
@@ -766,7 +766,7 @@ class installer_dist extends installer_base {
 		$content = str_replace('{ispconfig_log_priority}', $conf['ispconfig_log_priority'], $content);
 		$content = str_replace('{language}', $conf['language'], $content);
 		$content = str_replace('{timezone}', $conf['timezone'], $content);
-		$content = str_replace('{theme}', $conf['theme'], $content)
+		$content = str_replace('{theme}', $conf['theme'], $content);
 
 		wf("$install_dir/server/lib/$configfile", $content);
 		
