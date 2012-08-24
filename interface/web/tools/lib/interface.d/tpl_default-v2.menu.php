9909dba20698666711c4c1a02b7493b287fe10a5
Croydon <Croydon@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-08-24 11:32:11 +0000

    Removed obsolete default-v2 files Corrected paths in main tpl
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3416 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/tools/lib/interface.d/tpl_default-v2.menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$items[] = array(   'title'     => 'Default-v2 Theme',
-                    'target' 	=> 'content',
-                    'link'	=> 'tools/tpl_default-v2.php',
-                    'html_id'   => 'tpl_default-v2');
-?>
