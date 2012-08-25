4864c2fb49b91b0e65afaf628ccde48b4f1a57b6
cfoe <cfoe@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-08-25 14:30:31 +0000

    deactivated settings page for default theme because of the lack of settings for the end-user
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/trunk@3423 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/tools/lib/interface.d/tpl_default.menu.php
+++ interface/web/tools/lib/interface.d/tpl_default.menu.php
@@ -1,7 +1,8 @@
 <?php
 
-$items[] = array(   'title'     => 'Default Theme',
-                    'target' 	=> 'content',
-                    'link'	=> 'tools/tpl_default.php',
-                    'html_id'   => 'tpl_default');
+// No settings yet
+//$items[] = array(   'title'     => 'Default Theme',
+//                    'target' 	=> 'content',
+//                    'link'	=> 'tools/tpl_default.php',
+//                    'html_id'   => 'tpl_default');
 ?>
--- interface/web/tools/tpl_default.php
+++ interface/web/tools/tpl_default.php
@@ -28,15 +28,7 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 
-/******************************************
-* Begin Form configuration
-******************************************/
-
-$tform_def_file = "form/tpl_default.tform.php";
-
-/******************************************
-* End Form configuration
-******************************************/
+global $app, $conf;
 
 require_once('../../lib/config.inc.php');
 require_once('../../lib/app.inc.php');
@@ -44,65 +36,17 @@ require_once('../../lib/app.inc.php');
 //* Check permissions for module
 $app->auth->check_module_permissions('tools');
 
-// Loading classes
-$app->uses('tpl,tform,tform_actions');
-$app->load('tform_actions');
+$app->uses('tpl');
 
-class page_action extends tform_actions {
-	
-	function onLoad() {
-                global $app, $conf, $tform_def_file;
+$app->tpl->newTemplate('listpage.tpl.htm');
+$app->tpl->setInclude('content_tpl', 'templates/tpl_default.htm');
 
-                // Loading template classes and initialize template
-                if(!is_object($app->tpl)) $app->uses('tpl');
-                if(!is_object($app->tform)) $app->uses('tform');
+$lng_file = 'lib/lang/'.$_SESSION['s']['language'].'_tpl_default.lng';
+include($lng_file);
 
-                $app->tpl->newTemplate("tabbed_form.tpl.htm");
+$app->tpl->setVar($wb);
 
-                // Load table definition from file
-                $app->tform->loadFormDef($tform_def_file);
-				
-				// Importing ID
-                $this->id = $_SESSION['s']['user']['userid'];
-		$_POST['id'] = $_SESSION['s']['user']['userid'];
-
-                if(count($_POST) > 1) {
-                        $this->dataRecord = $_POST;
-                        $this->onSubmit();
-                } else {
-                        $this->onShow();
-                }
-        }
-        
-	function onBeforeInsert() {
-		global $app, $conf;
-		
-		if(!in_array($this->dataRecord['startmodule'],$this->dataRecord['modules'])) {
-			$app->tform->errorMessage .= $app->tform->wordbook['startmodule_err'];
-		}
-	}
-        
-	function onInsert() {
-		die('No inserts allowed.');
-	}
-		
-	function onBeforeUpdate() {
-		global $app, $conf;
-		
-		if($conf['demo_mode'] == true && $this->id <= 3) $app->tform->errorMessage .= 'This function is disabled in demo mode.';
-		
-		$_SESSION['s']['user']['language'] = $_POST['language'];
-		$_SESSION['s']['language'] = $_POST['language'];
-                
-                if(@is_array($this->dataRecord['modules']) && !in_array($this->dataRecord['startmodule'],$this->dataRecord['modules'])) {
-			$app->tform->errorMessage .= $app->tform->wordbook['startmodule_err'];
-		}
-	}
-	
-	
-}
-
-$page = new page_action;
-$page->onLoad();
+$app->tpl_defaults();
+$app->tpl->pparse();
 
 ?>
