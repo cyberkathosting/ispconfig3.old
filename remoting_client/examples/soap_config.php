0d473ee72176eb4b1a6996c927fe6f1f1aee950e
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-10-25 13:40:40 +0000

    Corrected defaults in soap config file.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/branches/ispconfig-3.0.5@3605 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- remoting_client/examples/soap_config.php
+++ remoting_client/examples/soap_config.php
@@ -3,12 +3,12 @@
 $username = 'admin';
 $password = 'admin';
 
-
+/*
 $soap_location = 'http://localhost:8080/ispconfig3_3.0.5/interface/web/remote/index.php';
 $soap_uri = 'http://localhost:8080/ispconfig3_3.0.5/interface/web/remote/';
+*/
 
-/*
 $soap_location = 'http://192.168.0.105:8080/remote/index.php';
 $soap_uri = 'http://192.168.0.105:8080/remote/';
-*/
+
 ?>
