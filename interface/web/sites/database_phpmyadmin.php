443bebca5f05b999233a44b133a38de529c869ef
Croydon <Croydon@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-11-02 10:18:18 +0000

    Updated:  - allowed some more characters in phpmyadmin url (system config) like redirect urls do  - added [DATABASENAME] as placeholder to the phpmyadmin url to be able to use urls like http://[SERVERNAME]/?db=[DATABASENAME]
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/branches/ispconfig-3.0.5@3638 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- interface/web/sites/database_phpmyadmin.php
+++ interface/web/sites/database_phpmyadmin.php
@@ -45,7 +45,7 @@ $databaseId = $app->functions->intval($_GET['id']);
 /*
  * Get the data to connect to the database
  */
-$dbData = $app->db->queryOneRecord("SELECT server_id FROM web_database WHERE database_id = " . $databaseId);
+$dbData = $app->db->queryOneRecord("SELECT server_id, database_name FROM web_database WHERE database_id = " . $databaseId);
 $serverId = $app->functions->intval($dbData['server_id']);
 if ($serverId == 0){
     die ("No DB-Server found!");
@@ -64,7 +64,7 @@ $web_config = $app->getconf->get_server_config($serverId,'web');
  
 if($global_config['phpmyadmin_url'] != '') {
 	$phpmyadmin_url = $global_config['phpmyadmin_url'];
-	$phpmyadmin_url = str_replace('[SERVERNAME]',$serverData['server_name'],$phpmyadmin_url);
+	$phpmyadmin_url = str_replace(array('[SERVERNAME]', '[DATABASENAME]'),array($serverData['server_name'], $dbData['database_name']),$phpmyadmin_url);
 	header('Location:'.$phpmyadmin_url);
 } else {
 	isset($_SERVER['HTTPS'])? $http = 'https' : $http = 'http';
