b60a9219691751a70c67239bd848f83b750ee6c4
till <till@c1a6f2c7-8504-0410-933a-a4dae0184b0a>
2012-11-20 15:01:59 +0000

    Set default umask to 022 in server.sh file.
    git-svn-id: svn://svn.ispconfig.org/ispconfig3/branches/ispconfig-3.0.5@3683 c1a6f2c7-8504-0410-933a-a4dae0184b0a

--- server/server.sh
+++ server/server.sh
@@ -4,6 +4,8 @@ PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/X11R6/bin
 
 . /etc/profile
 
+umask 022
+
 if [ -f /usr/local/ispconfig/server/lib/php.ini ]; then
         PHPINIOWNER=`stat -c %U /usr/local/ispconfig/server/lib/php.ini`
         if [ $PHPINIOWNER == 'root' ] || [ $PHPINIOWNER == 'ispconfig'  ]; then
